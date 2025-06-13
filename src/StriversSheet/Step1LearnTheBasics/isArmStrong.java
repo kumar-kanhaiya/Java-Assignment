a7ec2869f052f5c62a5140fbe647f7101572d8f3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 12:44:45 +0530

    code

---
+++ src/StriversSheet/Step1LearnTheBasics/isArmStrong.java
@@ -0,0 +1,13 @@
+package StriversSheet.Step1LearnTheBasics;
+
+import java.util.Scanner;
+
+public class isArmStrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int number = input.nextInt();
+
+    }
+    public static boolean
+}
