b332abf10a210b11ff6b3313a48cc77c2c0ef680
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 11:34:41 +0530

    code

--- src/StriversSheet/Step1LearnTheBasics/reverseNumber.java
+++ src/StriversSheet/Step1LearnTheBasics/reverseNumber.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class reverseNumber {
     public static void main(String[] args) {
-        Scanner input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int number = input.nextInt();
+        System.out.println("Number reverse = " + reverse(number));
 
     }
     public static int reverse(int number){
