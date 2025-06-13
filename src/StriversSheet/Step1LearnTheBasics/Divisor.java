ad5a6f83b09965f7e8b3d4704284ad532ecec36c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 12:51:55 +0530

    code

---
+++ src/StriversSheet/Step1LearnTheBasics/Divisor.java
@@ -0,0 +1,17 @@
+package StriversSheet.Step1LearnTheBasics;
+
+import java.util.Scanner;
+
+public class Divisor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number ;
+        System.out.print("Enter your number : ");
+        number = input.nextInt();
+        for (int i = 1; i <= number ; i++) {
+            if(number%i == 0){
+                System.out.printf("%d , ",i);
+            }
+        }
+    }
+}
