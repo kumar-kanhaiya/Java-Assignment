9e0cc31604e1593dc406db5b110140d08620a798
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 14:22:17 +0530

    code

--- src/StriversSheet/Step1LearnTheBasics/Recusion/printingSomething.java
+++ src/StriversSheet/Step1LearnTheBasics/Recusion/printingSomething.java
@@ -2,6 +2,16 @@ package StriversSheet.Step1LearnTheBasics.Recusion;
 
 public class printingSomething {
     public static void main(String[] args) {
+        System.out.println(printingNumber(1));
 
+    }
+
+
+    public static int printingNumber(int number) {
+        if (number == 15) {
+            return 15;
+        }
+        System.out.print(number + " , ");
+        return printingNumber(number + 1);
     }
 }
