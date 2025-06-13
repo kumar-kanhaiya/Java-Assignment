1a19c786c83c43bf5daa19b1e6d62b0e91d22f6c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 09:27:28 +0530

    code

---
+++ src/JAVACHALLENGE100Days/Day7/NonRepeatingCharacter.java
@@ -0,0 +1,11 @@
+package JAVACHALLENGE100Days.Day7;
+
+import java.util.Scanner;
+
+public class NonRepeatingCharacter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+
+    }
+}
