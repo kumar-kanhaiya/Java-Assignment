a2a5fdcad7f6aa8ba9a58fd7bc6cd8387014224f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 12:31:12 +0530

    code

--- src/StriversSheet/Step1LearnTheBasics/GCD.java
+++ src/StriversSheet/Step1LearnTheBasics/GCD.java
@@ -4,4 +4,13 @@ public class GCD {
     public static void main(String[] args) {
 
     }
+    public static int  GCD(int num1 , int num2){
+
+    }
+    public static int minimum(int num1 , int num2){
+        if(num1 < num2){
+            return num1;
+        }
+        return num2;
+    }
 }
