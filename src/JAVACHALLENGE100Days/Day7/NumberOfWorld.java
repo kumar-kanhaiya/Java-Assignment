f8cbe31ca0ade05edd1337ef35e6339f22aa3fae
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-13 09:15:57 +0530

    code

---
+++ src/JAVACHALLENGE100Days/Day7/NumberOfWorld.java
@@ -0,0 +1,19 @@
+package JAVACHALLENGE100Days.Day7;
+
+import java.util.Scanner;
+
+public class NumberOfWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Sentence : ");
+        String sentence = input.nextLine();
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            char index = sentence.charAt(i);
+            if(index == ' ' || i == sentence.length()-1){
+                count++;
+            }
+        }
+        System.out.printf("Number of words = %d " , count);
+    }
+}
