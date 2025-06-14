ee44c1d2d2cc985695590de29bba01921ba031cf
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-14 09:33:18 +0530

    code

--- src/StriversSheet/Step1LearnTheBasics/Recusion/ReverseArray.java
+++ src/StriversSheet/Step1LearnTheBasics/Recusion/ReverseArray.java
@@ -1,12 +1,13 @@
 package StriversSheet.Step1LearnTheBasics.Recusion;
 
+import java.util.Arrays;
+
 public class ReverseArray {
     public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
 
     }
-    public static void reverse(int[] array , int start , int end ){
-        int[] newArray = new int[array.length];
-        // base case
+    public static void reverse(int[] array , int n){
 
     }
 }
