a358a48ec8f27dab7d92fb856ba3dab86e76f428
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-14 09:37:30 +0530

    code

--- src/StriversSheet/Step1LearnTheBasics/Recusion/ReverseArray.java
+++ src/StriversSheet/Step1LearnTheBasics/Recusion/ReverseArray.java
@@ -5,9 +5,15 @@ import java.util.Arrays;
 public class ReverseArray {
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5};
+        reverse(array , array.length - 1 );
 
     }
     public static void reverse(int[] array , int n){
-
+        if(n == 0){
+            System.out.print(array[0]);
+            return;
+        }
+        System.out.print(array[n] + " ");
+        reverse(array , n-1);
     }
 }
