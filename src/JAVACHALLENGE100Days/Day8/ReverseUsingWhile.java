55964a980a09a591771d9ffa07a1947b05e93396
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-14 14:17:37 +0530

    code

--- src/JAVACHALLENGE100Days/Day8/ReverseUsingWhile.java
+++ src/JAVACHALLENGE100Days/Day8/ReverseUsingWhile.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class ReverseUsingWhile {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int number = input.nextInt();
+        int reverse = 0;
+        while(number !=0){
+            int rem = number%10;
+            reverse = reverse*10 + rem;
+            number /=10;
+        }
+        System.out.println(reverse);
     }
 }
