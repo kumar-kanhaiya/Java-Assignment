1493e8476585cf850b50b8011ff0ca53897a5ff2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-14 14:21:10 +0530

    code

--- src/JAVACHALLENGE100Days/Day8/Problem_2.java
+++ src/JAVACHALLENGE100Days/Day8/Problem_2.java
@@ -2,6 +2,19 @@ package JAVACHALLENGE100Days.Day8;
 
 public class Problem_2 {
     public static void main(String[] args) {
+        int[][] array = {{1,9,3},{4,5,6}};
+        sout
 
+    }
+    public static int largestElement(int[][] array ){
+        int max = 0;
+        for (int i = 0; i < array.length ; i++) {
+            for (int j = 0; j < array[i].length ; j++) {
+                if(array[i][j] > max){
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
     }
 }
