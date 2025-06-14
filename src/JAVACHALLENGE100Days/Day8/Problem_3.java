52563913da55d23fee072717d3cc617f94dcda91
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-14 14:28:14 +0530

    code

--- src/JAVACHALLENGE100Days/Day8/Problem_3.java
+++ src/JAVACHALLENGE100Days/Day8/Problem_3.java
@@ -2,6 +2,18 @@ package JAVACHALLENGE100Days.Day8;
 
 public class Problem_3 {
     public static void main(String[] args) {
+        // repeated word in String
 
+    }
+    public static void repeated(String input){
+        input = input.toLowerCase();
+        for (int i = 0; i <input.length() ; i++) {
+            char index = input.charAt(i);
+            for (int j = i+1; j < input.length() ; j++) {
+                if(index == input.charAt(j)){
+                    System.out.print(index);
+                }
+            }
+        }
     }
 }
