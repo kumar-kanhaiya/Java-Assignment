7c45504abdb296183e219ae1be6b0974cf1bf440
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-15 09:17:10 +0530

    code

--- src/StriversSheet/Step3Array/SecondLargest.java
+++ src/StriversSheet/Step3Array/SecondLargest.java
@@ -2,8 +2,21 @@ package StriversSheet.Step3Array;
 
 public class SecondLargest {
     public static void main(String[] args) {
-        int[] array = {8,8,7,6,5};
-
+        int[] array = {8,8,8,8,8,8};
+        System.out.println(secondMax(array));
+    }
+    public static int secondMax(int[] array ){
+        int max = max(array);
+        int ans = -1;
+        for (int i = 0; i < array.length ; i++) {
+            if(array[i] == max){
+                continue;
+            }
+            if(array[i] > ans){
+                ans = array[i];
+            }
+        }
+        return ans;
     }
     public static  int max(int[] array ){
         int max = -1;
