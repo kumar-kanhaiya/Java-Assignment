9c9af8e07f19d74954298f0d4588414257540770
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-15 12:40:22 +0530

    code

--- src/StriversSheet/Step3Array/isSorted.java
+++ src/StriversSheet/Step3Array/isSorted.java
@@ -2,14 +2,22 @@ package StriversSheet.Step3Array;
 // leetcode - 1752
 public class isSorted {
     public static void main(String[] args) {
-        int[] array = {3,4,5,1,2};
+        int[] array = {2,1,3,4};
         System.out.println(check(array));
     }
     public static boolean check(int[] array) {
         for (int i = 0; i < array.length ; i++) {
             for (int j = i+1; j < array.length ; j++) {
                 if(array[j-1] > array[j]){
-                    return false;
+                    if(array[j] > array[0] || array[array.length-1]> array[0]){
+                        return false;
+                    }
+                    else if (j == array.length -1 ){
+                        return true;
+                    }
+                    else{
+                        continue;
+                    }
                 }
             }
         }
