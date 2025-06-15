2b64a12353678ff7d30dbf58c1f91b8b2db95d2c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-15 09:07:36 +0530

    code

--- src/StriversSheet/Step3Array/largestElement.java
+++ src/StriversSheet/Step3Array/largestElement.java
@@ -2,6 +2,16 @@ package StriversSheet.Step3Array;
 
 public class largestElement {
     public static void main(String[] args) {
+    int[] array = {3,3,6,1};
 
+    }
+    public static int largest(int[] array ){
+        int max = array[0];
+        for (int i = 0; i < array.length ; i++) {
+            if(max < array[i]){
+                max = array[i];
+            }
+        }
+        return max;
     }
 }
