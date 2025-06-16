7ec7f368f1b668c2fa8f21c99cdad78f979abded
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-16 14:09:27 +0530

    code

--- src/StriversSheet/Step3Array/linearSearch.java
+++ src/StriversSheet/Step3Array/linearSearch.java
@@ -4,5 +4,12 @@ public class linearSearch {
     public static void main(String[] args) {
 
     }
-
+    public static int index(int[] array , int target){
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
