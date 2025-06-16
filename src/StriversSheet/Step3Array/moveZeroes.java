858acda4d3f98ef3695b4133fc1287a929913d1e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-16 14:06:20 +0530

    code

--- src/StriversSheet/Step3Array/moveZeroes.java
+++ src/StriversSheet/Step3Array/moveZeroes.java
@@ -8,25 +8,36 @@ public class moveZeroes {
         moveZero(array);
         System.out.println(Arrays.toString(array));
     }
-    public static void moveZero(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length; j++) {
-                if( array[array.length -1]==0){
-                    break;
-                }
-                if(array[i] == 0 && array[j] != 0 ){
-                    swap(array,i,j);
-
-                }
-                if(array[i] == 0 && array[j+1] != 0 && j < array.length-1){
-                    swap(array,i,j+1);
+    public static void moveZero(int[] array ) {
+        for (int i = 0; i < array.length ; i++) {
+            int c = array[i];
+            for (int j = i+1; j < array.length ; j++) {
+                if(array[j] != 0 && c == 0){
+                    swap(array,i, j);
                 }
             }
         }
     }
+//    public static void moveZero(int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i+1; j < array.length; j++) {
+//                if( array[array.length -1]==0){
+//                    break;
+//                }
+//                if(array[i] == 0 && array[j] != 0 ){
+//                    swap(array,i,j);
+//
+//                }
+//                if(array[i] == 0 && array[j+1] != 0 && j < array.length-1){
+//                    swap(array,i,j+1);
+//                }
+//            }
+//        }
+
     public static void swap(int[] array , int first , int second){
         int temp = array[first];
         array[first] = array[second];
         array[second] = temp;
     }
-}
+
+    }
---
+++ src/StriversSheet/Step3Array/unionOfArray.java
@@ -0,0 +1,7 @@
+package StriversSheet.Step3Array;
+
+public class unionOfArray {
+    public static void main(String[] args) {
+
+    }
+}
