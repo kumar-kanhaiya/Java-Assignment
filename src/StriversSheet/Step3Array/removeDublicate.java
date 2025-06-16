9b23aa18994775b7fc820dc63035b933f5efbb85
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-16 10:19:54 +0530

    code

--- src/StriversSheet/Step3Array/removeDublicate.java
+++ src/StriversSheet/Step3Array/removeDublicate.java
@@ -1,8 +1,23 @@
 package StriversSheet.Step3Array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class removeDublicate {
     public static void main(String[] args) {
         int[] array = {1,1,2};
+        nonDublicate(array);
     }
-    public static
+    public static void nonDublicate(int[] array ){
+        Set<Integer> ans = new HashSet<>();
+        for (int i = 0; i < array.length ; i++) {
+            ans.add(array[i]);
+
+        }
+        Object[] arr = ans.toArray();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+    }
 }
