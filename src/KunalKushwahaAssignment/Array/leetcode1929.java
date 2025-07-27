6f3f3f126aa85bc28e36e3ade0c24f3747851565
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-27 21:13:58 +0530

    code

--- src/KunalKushwahaAssignment/Array/leetcode1929.java
+++ src/KunalKushwahaAssignment/Array/leetcode1929.java
@@ -4,4 +4,9 @@ public class leetcode1929 {
     public static void main(String[] args) {
 
     }
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2* nums.length];
+
+        return ans;
+    }
 }
