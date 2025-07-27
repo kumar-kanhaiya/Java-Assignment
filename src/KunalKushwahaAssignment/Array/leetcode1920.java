fa4988dea3622fd67422d9a8cba9d2cae3e4ec75
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-27 21:06:50 +0530

    code

--- src/KunalKushwahaAssignment/Array/leetcode1920.java
+++ src/KunalKushwahaAssignment/Array/leetcode1920.java
@@ -1,7 +1,22 @@
 package KunalKushwahaAssignment.Array;
 
+import java.util.Arrays;
+
 public class leetcode1920 {
     public static void main(String[] args) {
-
+    int[] array = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(buildArray(array)));
+    }
+    public static int[] buildArray(int[] nums){
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length ; i++) {
+            int check = nums[i];
+            for (int j = 0; j < nums.length ; j++) {
+                if(check == j){
+                    ans[i] = nums[j];
+                }
+            }
+        }
+        return ans;
     }
 }
