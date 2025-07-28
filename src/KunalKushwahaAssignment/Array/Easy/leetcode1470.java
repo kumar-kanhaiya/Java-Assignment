7cec9b02de0e2c0d80a3554a0ba332167100d8c2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 08:10:40 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1470.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1470.java
@@ -1,7 +1,19 @@
 package KunalKushwahaAssignment.Array.Easy;
 
+import java.util.Arrays;
+
 public class leetcode1470 {
     public static void main(String[] args) {
+        int[] array = {2,5,1,3,4,7};
+        System.out.println(Arrays.toString(shuffle(array,3)));
 
+    }
+    public static int[] shuffle(int[] nums, int n) {
+    int[] ans = new int[nums.length];
+        for (int i = 0; i < n ; i++) {
+            ans[2*i] = nums[i];
+            ans[2*i + 1] = nums[i+n];
+        }
+        return ans;
     }
 }
