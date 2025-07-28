27a151da6d6a1e28e3adca24ea86e695590dab53
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 10:52:13 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode832.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode832.java
@@ -1,7 +1,31 @@
 package KunalKushwahaAssignment.Array.Easy;
 
+import java.util.Arrays;
+
 public class leetcode832 {
     public static void main(String[] args) {
+        int[][] array = {{1,1,0},{1,0,1},{0,0,0}};
+        flipAndInvertImage(array);
 
     }
+    public static void flipAndInvertImage(int[][] image) {
+        for (int i = 0; i < image.length ; i++) {
+            int left = 0;
+            int right = image.length - 1;
+            while(left <right){
+                int temp = image[i][left];
+                image[i][left] = image[i][right];
+                image[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        for (int i = 0; i < image.length ; i++) {
+            for (int j = 0; j <image[i].length ; j++) {
+                System.out.print(image[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
