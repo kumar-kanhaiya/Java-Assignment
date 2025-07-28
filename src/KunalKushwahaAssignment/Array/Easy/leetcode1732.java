ef90e05461068cb9085a21bde130608381aa7ac6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 10:06:50 +0530

    code

---
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1732.java
@@ -0,0 +1,7 @@
+package KunalKushwahaAssignment.Array.Easy;
+
+public class leetcode1732 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/KunalKushwahaAssignment/Array/Easy/leetcode1832.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1832.java
@@ -2,6 +2,25 @@ package KunalKushwahaAssignment.Array.Easy;
 
 public class leetcode1832 {
     public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpso";
+        System.out.println(checkIfPangram(sentence));
 
+    }
+    public static boolean checkIfPangram(String sentence) {
+        for (int i = 0; i < 26 ; i++) {
+            int j = 0;
+            boolean isCheck = false;
+            while(j<sentence.length()){
+                if(i+97 == (int) sentence.charAt(j)){
+                    isCheck = true;
+                    break;
+                }
+                j++;
+            }
+            if(isCheck ==false){
+                return false;
+            }
+        }
+        return true;
     }
 }
