52a3a68cd0c0dd00fee821b74cc1c0e21d9e596e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 08:51:56 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1512.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1512.java
@@ -2,10 +2,19 @@ package KunalKushwahaAssignment.Array.Easy;
 
 public class leetcode1512 {
     public static void main(String[] args) {
+        int[] array = {1,1,1,1};
+        System.out.println(numIdenticalPairs(array));
 
     }
     public static int numIdenticalPairs(int[] nums) {
         int count = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = i+1; j <nums.length ; j++) {
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
         return count;
     }
 }
