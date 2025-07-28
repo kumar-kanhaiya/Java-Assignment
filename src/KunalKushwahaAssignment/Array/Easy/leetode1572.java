3276e1f574fe128b1dde25e4565fb880c0878804
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 11:14:07 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetode1572.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetode1572.java
@@ -2,6 +2,25 @@ package KunalKushwahaAssignment.Array.Easy;
 
 public class leetode1572 {
     public static void main(String[] args) {
+        int[][] array = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(diagonalSum(array));
 
+    }
+    public static int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.length ; i++) {
+            for (int j = 0; j <mat[i].length ; j++) {
+                if(i == j){
+                    sum += mat[i][j];
+                }
+                if(i == mat.length-1 && j==0){
+                    sum += mat[i][j];
+                }
+                if(j == mat.length - 1 && i ==0){
+                    sum+= mat[i][j];
+                }
+            }
+        }
+        return sum;
     }
 }
