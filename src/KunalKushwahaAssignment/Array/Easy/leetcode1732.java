d6400a3cc53b817929dfe50f6e3230fa39fedb5f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 10:22:40 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1732.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1732.java
@@ -1,7 +1,29 @@
 package KunalKushwahaAssignment.Array.Easy;
 
+import java.util.Arrays;
+
 public class leetcode1732 {
     public static void main(String[] args) {
+        int[] array = {-5,1,5,0,-7};
+        System.out.println(largestAltitude(array));
 
+    }
+    public static int largestAltitude(int[] gain) {
+        int[] ans = new int[gain.length + 1];
+        ans[0] = 0;
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] = ans[i-1] + gain[i-1];
+
+        }
+        return helper(ans);
+    }
+    public static int helper(int[] array ){
+        int max = 0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
     }
 }
