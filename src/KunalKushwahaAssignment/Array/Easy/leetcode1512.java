2e76ff881fc3662ceaac50232131cf174933f7c3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 08:50:08 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1512.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1512.java
@@ -4,4 +4,8 @@ public class leetcode1512 {
     public static void main(String[] args) {
 
     }
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        return count;
+    }
 }
