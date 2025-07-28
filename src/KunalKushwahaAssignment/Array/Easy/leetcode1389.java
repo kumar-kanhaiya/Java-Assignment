fb19a8e53fe762f133cee98ed12ae0c93cfbc429
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 09:40:00 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1389.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1389.java
@@ -1,7 +1,16 @@
 package KunalKushwahaAssignment.Array.Easy;
 
+import java.util.Arrays;
+
 public class leetcode1389 {
     public static void main(String[] args) {
+        int[] array = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        System.out.println(Arrays.toString(createTargetArray(array,index)));
+
+    }
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        int[] ans = new int[nums.length];
 
     }
 }
