dff658dc1eefb31bbd806d18faf0bfe5a5d927e6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 11:41:38 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1295.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1295.java
@@ -2,6 +2,32 @@ package KunalKushwahaAssignment.Array.Easy;
 
 public class leetcode1295 {
     public static void main(String[] args) {
+        int[] array = {12,345,2,6,7896};
+        System.out.println(findNumbers(array));
 
+    }
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i <nums.length ; i++) {
+            boolean check = isEven(nums[i]);
+            if(check ==true){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static boolean isEven(int number){
+        int count = 0;
+        while(number > 0){
+            int rem = number/10;
+            count++;
+            number/=10;
+
+        }
+        if(count%2==0){
+            return true;
+        }
+        return false;
     }
 }
