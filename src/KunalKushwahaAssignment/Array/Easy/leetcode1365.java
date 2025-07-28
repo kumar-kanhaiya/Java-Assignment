f72eed0a959da0e20a8e42e9c051c290d1857da4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-28 09:24:16 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode1365.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1365.java
@@ -1,7 +1,26 @@
 package KunalKushwahaAssignment.Array.Easy;
 
+import java.util.Arrays;
+
 public class leetcode1365 {
     public static void main(String[] args) {
+        int[] array = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(array)));
 
+    }
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length ; i++) {
+            int count = 0;
+            for (int j = 0; j <nums.length ; j++) {
+                if(nums[i] > nums[j]){
+                    count++;
+                }
+                ans[i] = count;
+            }
+
+        }
+        return ans;
     }
 }
---
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1389.java
@@ -0,0 +1,7 @@
+package KunalKushwahaAssignment.Array.Easy;
+
+public class leetcode1389 {
+    public static void main(String[] args) {
+
+    }
+}
