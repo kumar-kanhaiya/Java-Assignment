02d5d955b57276d05339f4e62f30b88d54c89607
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-29 19:02:29 +0530

    code

---
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode1854.java
@@ -0,0 +1,7 @@
+package KunalKushwahaAssignment.Array.Easy;
+
+public class leetcode1854 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/KunalKushwahaAssignment/Array/Easy/leetcode989.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode989.java
@@ -2,6 +2,7 @@ package KunalKushwahaAssignment.Array.Easy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class leetcode989 {
@@ -12,23 +13,35 @@ public class leetcode989 {
 
 
     }
-    public static List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> list = new ArrayList<>();
-        int i = 0;
-        int reverse = 0;
-        while (i< num.length){
-            reverse = reverse*10 + num[i]  ;
-            i++;
-        }
-        int all = reverse+k;
-        String str = String.valueOf(all);
-        for (char ch : str.toCharArray()) {
-            list.add(ch - '0');
+//    public static List<Integer> addToArrayForm(int[] num, int k) {
+//        List<Integer> list = new ArrayList<>();
+//        int i = 0;
+//        int reverse = 0;
+//        while (i< num.length){
+//            reverse = reverse*10 + num[i]  ;
+//            i++;
+//        }
+//        int all = reverse+k;
+//        String str = String.valueOf(all);
+//        for (char ch : str.toCharArray()) {
+//            list.add(ch - '0');
+//        }
+//        return list;
+//    }
+
+    // second approach
+    public static List<Integer> addToArrayForm(int[] nums , int k ){
+        LinkedList<Integer> list = new LinkedList<>();
+        int i = nums.length - 1 ;
+        while(i>=0 || k>0){
+            if(i>=0){
+                k +=nums[i];
+                i--;
+            }
+            list.addFirst(k%10);
+            k /=10;
         }
         return list;
     }
-
-    // second approach
-
 
 }
