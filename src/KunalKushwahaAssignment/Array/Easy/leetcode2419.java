dddfdca2369b990270d9709fb63b1754670f8853
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-30 10:05:52 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode2419.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode2419.java
@@ -2,17 +2,16 @@ package KunalKushwahaAssignment.Array.Easy;
 
 public class leetcode2419 {
     public static void main(String[] args) {
+        int[] array = {1,2,3,3,2,1};
+        System.out.println(longestSubarray(array));
 
     }
 
     public static int longestSubarray(int[] nums) {
 
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if((nums[i] & 1) > 0){
-
-            }
-        }
+        int max = max(nums);
+        int occ = occurence(nums , max);
+        return occ;
     }
     public static int max(int[] array ){
         int maz = array[0];
@@ -22,6 +21,21 @@ public class leetcode2419 {
             }
         }
         return maz;
+    }
+    public static int occurence(int[] array , int target){
+        int correntLength = 0;
+        int maxlength = 0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i] == target){
+                correntLength++;
+                maxlength = Math.max(correntLength , maxlength);
+            }
+            else{
+                correntLength = 0;
+            }
+        }
+        return maxlength;
+
     }
 
 }
