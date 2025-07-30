9aa4a0939c70c802a73189cfc66cf39deeb64161
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-30 07:29:25 +0530

    code

--- src/KunalKushwahaAssignment/Array/Easy/leetcode66.java
+++ src/KunalKushwahaAssignment/Array/Easy/leetcode66.java
@@ -36,5 +36,17 @@ public class leetcode66 {
 //        }
 //        return ans;
 //    }
+    public static int[] plusOne(int[] digits){
+        for (int i = digits.length - 1; i >=0 ; i--) {
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length  + 1];
+        result[0] = 1;
+        return result;
 
+    }
 }
