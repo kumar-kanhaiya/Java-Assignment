3a3a2428e0eed8aa8a46d93b0538159ad53bed8a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-06 11:32:11 +0530

    code

--- src/KunalKushwahaAssignment/Array/Medium/Problem59.java
+++ src/KunalKushwahaAssignment/Array/Medium/Problem59.java
@@ -1,4 +1,41 @@
 package KunalKushwahaAssignment.Array.Medium;
 
 public class Problem59 {
+    public static void main(String[] args) {
+
+    }
+    public int[][] generateMatrix(int n) {
+        int sRow = 0;
+        int sColl = 0;
+        int eRow = n-1;
+        int eColl = n-1;
+        int i = 1;
+        int[][] ans = new int[n][n];
+        while(sRow <= eRow && sColl <= eColl){
+            // for top case
+            for (int j = sColl; j <=eColl ; j++) {
+                ans[sRow][j] = i;
+
+            }
+
+            // for top to bottom
+            for (int j = sRow + 1; j >= eRow ; j++) {
+                ans[j][eColl] =i;
+            }
+
+            // for right to left
+            for (int j = eColl + 1; j >= sColl ; j--) {
+                if(sRow == eRow){
+                    break;
+                }
+                ans[eRow][j] = i;
+            }
+
+            // for bottom to up
+            for (int j = eRow+ 1 ; j >= sRow + 1 ; j--) {
+                if()
+            }
+
+        }
+    }
 }
