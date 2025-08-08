18458f1cc7be5afcdab7a63a53bd2c4c9938b25d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-08 14:07:17 +0530

    code

--- src/KunalKushwahaAssignment/Array/Medium/Problem238.java
+++ src/KunalKushwahaAssignment/Array/Medium/Problem238.java
@@ -9,24 +9,40 @@ public class Problem238 {
 
     }
     // good approach but time limit exceed 😒
-    public  static  int[] productExceptSelf(int[] nums) {
-        Queue<Integer> stack = new LinkedList<>();
-        int[] result = new int[nums.length];
-
-        for (int i = 0; i < nums.length ; i++) {
-            int ans = 1;
-            for (int j = 0; j < nums.length ; j++) {
-                if(i == j){
-                    continue;
-                }
-                ans *= nums[j];
-
+//    public  static  int[] productExceptSelf(int[] nums) {
+//        Queue<Integer> stack = new LinkedList<>();
+//        int[] result = new int[nums.length];
+//
+//        for (int i = 0; i < nums.length ; i++) {
+//            int ans = 1;
+//            for (int j = 0; j < nums.length ; j++) {
+//                if(i == j){
+//                    continue;
+//                }
+//                ans *= nums[j];
+//
+//            }
+//            stack.add(ans);
+//        }
+//        for (int i = 0; i < result.length ; i++) {
+//            result[i] = stack.remove();
+//        }
+//        return result;
+//    }
+    // this is not o(n) method
+    public  static  int[] productExceptSelf(int[] nums){
+        int i = 0 ;
+        int[] ans = new int[nums.length];
+        while(i< nums.length){
+            int left = 1;
+            int right = 1;
+            // multiply left item
+            for (int j = 1; j < nums.length ; j++) {
+                left *= nums[j-1];
             }
-            stack.add(ans);
+            ans[i] = left*right;
+            i++;
         }
-        for (int i = 0; i < result.length ; i++) {
-            result[i] = stack.remove();
-        }
-        return result;
+        return ans;
     }
 }
