2e11c3341e8e215a54db46f7901b412f1b01f816
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 00:24:58 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Problem167.java
+++ src/KunalKushwahaAssignment/Searching/Problem167.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class Problem167 {
     public static void main(String[] args) {
-        int[] array = {-1,0};
-        System.out.println(Arrays.toString(helper(array,0,-1)));
+        int[] array = {0,0,1,2};
+        System.out.println(Arrays.toString(twoSum(array,0)));
 
     }
-    public static int[] twoSum(int[] numbers, int target) {
+    public static int[] twoSum(int[] array, int target) {
         int start = 0;
         int end = array.length;
         while(start <= end){
             int mid = start  + (end - start)/2;
-            if(array[mid] + array[check] == target){
+            if(array[mid] + array[start] == target){
                 // we find our ans
-                return new int[]{check+1 , mid+1};
+                return new int[]{start+1 , mid+1};
             }
-            else if(array[mid] + array[check] > target){
+            else if(array[mid] + array[start] > target){
                 end = mid - 1;
             }
             else{
@@ -26,23 +26,6 @@ public class Problem167 {
         }
         return new int[]{-1,-1};
     }
-    }
-    public static int[] helper(int[] array , int check , int target ){
-        int start = 0;
-        int end = array.length;
-        while(start <= end){
-            int mid = start  + (end - start)/2;
-            if(array[mid] + array[check] == target){
-                // we find our ans
-                return new int[]{check+1 , mid+1};
-            }
-            else if(array[mid] + array[check] > target){
-                end = mid - 1;
-            }
-            else{
-                start = mid + 1;
-            }
-        }
-        return new int[]{-1,-1};
-    }
+
+
 }
