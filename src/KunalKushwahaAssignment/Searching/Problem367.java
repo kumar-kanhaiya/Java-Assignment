22dabbc3b3f206150cf00a56cbe0c9223483db5e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 00:42:23 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Problem367.java
+++ src/KunalKushwahaAssignment/Searching/Problem367.java
@@ -2,10 +2,30 @@ package KunalKushwahaAssignment.Searching;
 
 public class Problem367 {
     public static void main(String[] args) {
+        System.out.println(isPerfectSquare(5));
 
     }
     // this problem is done by using binary search
     public static boolean isPerfectSquare(int num) {
+        if(num <= 0){
+            return false;
+        }
+        int start = 1;
+        int end = num;
 
+        while(start <= end){
+            int mid = start + (end -start)/2;
+            if(mid*mid == num){
+                // we find our ans
+                return true;
+            }
+            else if (mid > num/mid){
+                end = mid -1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return false;
     }
 }
