7f9735cfc4f7f85fba7322590a130fec20fd1128
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-09 23:24:16 +0530

    code

--- src/KunalKushwahaAssignment/Searching/problem69.java
+++ src/KunalKushwahaAssignment/Searching/problem69.java
@@ -2,10 +2,29 @@ package KunalKushwahaAssignment.Searching;
 
 public class problem69 {
     public static void main(String[] args) {
+        System.out.println(mySqrt(2147395599));
+        System.out.println(Math.sqrt(2147395599));
 
     }
     // the solution is done by using binary search
     public static  int mySqrt(int x) {
-
+        int start = 0;
+        int end = x;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(mid < x/mid && (mid+1) > x/(mid+1) ){
+                return mid;
+            }
+            else if(mid > x/mid){
+                end = mid - 1;
+            }
+            else if(mid < x/mid){
+                start = mid +1 ;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
     }
 }
