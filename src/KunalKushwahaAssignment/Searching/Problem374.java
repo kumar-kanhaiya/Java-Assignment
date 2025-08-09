b00687404222c9eb667d9f87ac68220ae4378872
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-09 23:40:15 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Problem374.java
@@ -0,0 +1,33 @@
+package KunalKushwahaAssignment.Searching;
+
+public class Problem374 {
+    public static void main(String[] args) {
+
+    }
+    public static int guess(int number ){
+        return number;
+    }
+
+    /**
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is higher than the picked number
+     *			      1 if num is lower than the picked number
+     *               otherwise return 0
+     * int guess(int num);
+     */
+    // we use binary search for this
+    /*
+    steps
+    1. start point is 0 and end point is n
+    2. declaring mid then take conditions when the ans == mid then return mid
+     */
+    public static int guessNumber(int n) {
+        int start = 0;
+        int end = n;
+
+        while(start <= end){
+
+        }
+    }
+}
