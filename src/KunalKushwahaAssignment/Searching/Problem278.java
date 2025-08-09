2937ea363826beca468d06a7e3f6599684d4066a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-09 23:48:18 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Problem278.java
@@ -0,0 +1,18 @@
+package KunalKushwahaAssignment.Searching;
+
+public class Problem278 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isBadVersion(int version){
+        return true;
+    }
+    public static int firstBadVersion(int n) {
+        int start = 0;
+        int end = n;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+        }
+    }
+}
