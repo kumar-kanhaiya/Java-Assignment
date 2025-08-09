085ead365487bb9c225fa5d9aea193787a6513cf
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-09 22:00:27 +0530

    sqrt problem

--- src/KunalKushwahaAssignment/Array/Medium/problem198.java
+++ src/KunalKushwahaAssignment/Array/Medium/problem198.java
@@ -2,21 +2,26 @@ package KunalKushwahaAssignment.Array.Medium;
 
 public class problem198 {
     public static void main(String[] args) {
-        int[] array = {2,7,9,3,1};
+        int[] array = {1,2};
         System.out.println(rob(array));
 
     }
     public static int rob(int[] nums) {
-        int ans = 0;
+        if(nums.length == 0){
+            return -1;
+        }
+        int ans = nums[0];
         // chacking the row
         for (int i = 0; i < nums.length ; i++) {
-            int check = 0;
+            int check = nums[i];
             if(i%2 == 0 && i+2 < nums.length){
+                check = 0;
                 for (int j =i; j < nums.length ; j = j+2) {
                     check += nums[j];
                 }
             }
             if(i%2 !=0 && 2*i+1< nums.length){
+                check = 0;
                 for (int j = i; j < nums.length ; j = 2*j + 1 ) {
                     check += nums[j];
                 }
---
+++ src/KunalKushwahaAssignment/Searching/problem69.java
@@ -0,0 +1,11 @@
+package KunalKushwahaAssignment.Searching;
+
+public class problem69 {
+    public static void main(String[] args) {
+
+    }
+    // the solution is done by using binary search
+    public static  int mySqrt(int x) {
+
+    }
+}
