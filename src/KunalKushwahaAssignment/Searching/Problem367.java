7c1b60b85b0b4bb060c4c79c27586b720c42ec45
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 00:33:32 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Problem367.java
@@ -0,0 +1,11 @@
+package KunalKushwahaAssignment.Searching;
+
+public class Problem367 {
+    public static void main(String[] args) {
+
+    }
+    // this problem is done by using binary search
+    public static boolean isPerfectSquare(int num) {
+
+    }
+}
