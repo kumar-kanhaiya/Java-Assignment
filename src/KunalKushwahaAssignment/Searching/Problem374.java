4565a3a6940d7532dd00d97ff82a48ae758b2087
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-09 23:43:21 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Problem374.java
+++ src/KunalKushwahaAssignment/Searching/Problem374.java
@@ -22,12 +22,24 @@ public class Problem374 {
     1. start point is 0 and end point is n
     2. declaring mid then take conditions when the ans == mid then return mid
      */
+
+    //
     public static int guessNumber(int n) {
         int start = 0;
         int end = n;
 
         while(start <= end){
-
+            int mid = start + (end - start)/2;
+            if(guess(mid) == 0){
+                return mid;
+            }
+            else if (guess(mid) == 1){
+                start = mid +1 ;
+            }
+            else{
+                end = mid -1;
+            }
         }
+        return end;
     }
 }
