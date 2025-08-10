26e63fd82c68551f958f8133d2a4a56f116a4d38
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 23:39:00 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/problem1351.java
@@ -0,0 +1,10 @@
+package KunalKushwahaAssignment.Searching;
+
+public class problem1351 {
+    public static void main(String[] args) {
+
+    }
+    public static int countNegatives(int[][] grid) {
+
+    }
+}
