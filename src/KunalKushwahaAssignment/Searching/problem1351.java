fa40009d5e58ff6914906648b0762f775196d1f4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-11 00:00:57 +0530

    code

--- src/KunalKushwahaAssignment/Searching/problem1351.java
+++ src/KunalKushwahaAssignment/Searching/problem1351.java
@@ -2,9 +2,33 @@ package KunalKushwahaAssignment.Searching;
 
 public class problem1351 {
     public static void main(String[] args) {
+        int[][] array = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        System.out.println(countNegatives(array));
 
     }
-    public static int countNegatives(int[][] grid) {
+    public static int countNegatives(int[][] array) {
+        int count = 0;
+        int srow = 0;
+        int scoll = 0;
+        int erow = array.length - 1;
+        int ecoll = array[0].length - 1;
+        while(srow<= erow && scoll <= ecoll){
+            // chacking in call
+            for (int i = scoll; i <=ecoll ; i++) {
+                if(array[srow][i] < 0){
+                    count += 1;
+                }
+            }
+            // checking in row
+            for (int i = srow + 1; i <= erow; i++) {
+                if(array[i][scoll] < 0){
+                    count += 1;
+                }
+            }
+            srow++;
+            scoll++;
 
+        }
+        return count;
     }
 }
