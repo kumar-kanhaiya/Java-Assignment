17fe1d46c0423907dde224a412b171d5cceb9011
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-11 00:06:17 +0530

    leetcode problem 349

--- src/KunalKushwahaAssignment/Searching/problem349.java
+++ src/KunalKushwahaAssignment/Searching/problem349.java
@@ -1,7 +1,28 @@
 package KunalKushwahaAssignment.Searching;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class problem349 {
     public static void main(String[] args) {
 
+    }
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Queue<Integer> list = new LinkedList<>();
+        // we use two pointer
+        int start = 0;
+        int end = maxLength(nums1, nums2);
+        int index = 0;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+        }
+    }
+    public static int maxLength(int[] nums1 , int[] nums2){
+        if(nums1.length > nums2.length){
+            return nums1.length;
+        }
+        else{
+            return nums2.length;
+        }
     }
 }
