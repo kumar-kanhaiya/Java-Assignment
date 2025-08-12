31a051b4bb3e87865ffda86c3316fbb82969605e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-12 13:41:56 +0530

    codd

--- src/KunalKushwahaAssignment/Searching/problem349.java
+++ src/KunalKushwahaAssignment/Searching/problem349.java
@@ -6,16 +6,36 @@ import java.util.Queue;
 
 public class problem349 {
     public static void main(String[] args) {
-        int[] array = {1,2,2,3};
-        int[] ans = removeDublicate(array);
+        int[] array = {4,9,5};
+        int[] arr = {9,4,9,8,4};
+        int[] ans = intersection(array , arr);
         System.out.println(Arrays.toString(ans));
 
     }
-//    public int[] intersection(int[] nums1, int[] nums2) {
-//        Queue<Integer> list = new LinkedList<>();
-//        // we use two pointer
-//
-//    }
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Queue<Integer> list = new LinkedList<>();
+        sortArray(nums1);
+        sortArray(nums2);
+        int[] arr1 = removeDublicate(nums1);
+        int[] arr2 = removeDublicate(nums2);
+        for (int i = 0; i < arr1.length ; i++) {
+            for (int j = 0; j < arr2.length ; j++) {
+                if(arr1[i] == arr2[j]){
+                    list.add(arr1[i]);
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+        int size = list.size();
+        int[] ans = new int[size];
+        for (int i = 0; i < size; i++) {
+            ans[i] = list.remove();
+        }
+        return ans;
+
+    }
 
     public static int[] removeDublicate(int[] arr){
         Queue<Integer> queue = new LinkedList<>();
