03896919678f2d686df68a7ad75d3a597a5e8792
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-19 16:32:40 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/problem540.java
+++ src/KunalKushwahaAssignment/Searching/Medium/problem540.java
@@ -7,23 +7,6 @@ public class problem540 {
 
     }
     public static int singleNonDuplicate(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
 
-            if((nums[mid] > nums[mid-1] )&& nums[mid] < nums[mid + 1]){
-                // ans found
-                return nums[mid];
-            }
-            else if(nums[mid] >= nums[mid -1]){
-                start = start +1;
-            }
-            else{
-                end = end -1;
-            }
-
-        }
-        return -1;
     }
 }
