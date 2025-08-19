e880af31839cf2c014919c701d8944dab2f72cea
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-19 16:56:12 +0530

    codE

--- src/KunalKushwahaAssignment/Searching/Medium/problem540.java
+++ src/KunalKushwahaAssignment/Searching/Medium/problem540.java
@@ -16,26 +16,15 @@ public class problem540 {
         int e = nums.length -1 ;
         while(s < e){
             int mid = s + (e-s)/2;
-            if(mid%2 == 0){
-                if(nums[mid] == nums[mid + 1]){
-                    s = mid +1;
-                }
-                else{
-                    e = mid - 1;
-                }
-            }
-            else if(mid%2 !=0){
-                if(nums[mid] == nums[mid -1 ] ){
-                    s = mid + 1;
-                }
-                else{
-                    e = mid -1;
-                }
+            if(mid % 2 == 0 && nums[mid] == nums[mid+1] || mid%2 == 1 && nums[mid] == nums[mid -1]){
+                s = mid +1;
             }
             else{
-                return nums[mid];
+                e = mid;
             }
+
+
         }
-        return -1;
+        return nums[s];
     }
 }
