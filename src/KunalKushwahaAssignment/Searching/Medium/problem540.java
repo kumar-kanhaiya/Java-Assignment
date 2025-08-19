50c1737b308b40f0c329c90e6540d5bcfdf7c38b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-19 16:51:52 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/problem540.java
+++ src/KunalKushwahaAssignment/Searching/Medium/problem540.java
@@ -7,15 +7,35 @@ public class problem540 {
 
     }
     public static int singleNonDuplicate(int[] nums) {
+
         int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }
         int s = 0;
         int e = nums.length -1 ;
-        while(s <= e){
+        while(s < e){
             int mid = s + (e-s)/2;
-            int check = nums[mid] - nums[mid+1];
-            if(check == 0){
-                s = mid +1;
+            if(mid%2 == 0){
+                if(nums[mid] == nums[mid + 1]){
+                    s = mid +1;
+                }
+                else{
+                    e = mid - 1;
+                }
+            }
+            else if(mid%2 !=0){
+                if(nums[mid] == nums[mid -1 ] ){
+                    s = mid + 1;
+                }
+                else{
+                    e = mid -1;
+                }
+            }
+            else{
+                return nums[mid];
             }
         }
+        return -1;
     }
 }
