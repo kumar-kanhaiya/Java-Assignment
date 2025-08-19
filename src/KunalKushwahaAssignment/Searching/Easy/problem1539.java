e8e0af12a29fee6606cf436aa9ffafb94e7d9022
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-19 16:32:05 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Easy/problem1539.java
+++ src/KunalKushwahaAssignment/Searching/Easy/problem1539.java
@@ -8,18 +8,18 @@ public class problem1539 {
     }
     public static int findKthPositive(int[] arr, int k) {
         // first we do with root force approach
-        int ans = arr[0];
-        int start = 0;
-        while(k!= 0){
-
-            for(int i = arr[start] ; i< arr.length && i < arr[start + 1]  ; i++){
-                ans++;
-
+        int start = 1;
+        int i = 0;
+        while(k>0){
+            if(i< arr.length && start == arr[i]){
+                start++;
+                i++;
             }
-            start++;
-            k--;
-
+            else{
+                start++;
+                k--;
+            }
         }
-        return ans;
+        return start - 1;
     }
 }
