4abf9bb3328b8f43d5e0d7424b4206d84623f655
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-21 16:06:08 +0530

    cdoe

--- src/KunalKushwahaAssignment/Searching/Medium/Problem153.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem153.java
@@ -2,7 +2,8 @@ package KunalKushwahaAssignment.Searching.Medium;
 
 public class Problem153 {
     public static void main(String[] args) {
-        int[] array = {3,4,5,1,2};
+        int[] array = {4,5,6,7,0,1,2};
+        System.out.println(findMin(array));
 
 
     }
@@ -14,16 +15,17 @@ public class Problem153 {
             int mid = start + (end - start)/2;
 
             if(nums[mid] < ans){
-                ans = nums[mid];
+                ans = Math.min(ans, nums[mid]);
             }
-            if(nums[start] == nums[mid] && nums[end] == nums[mid]){
-                // case of repeating number
-                start++;
-                end--;
-                continue;
-            }
+
+//            if(nums[start] == nums[mid] && nums[end] == nums[mid]){
+//                // case of repeating number
+//                start++;
+//                end--;
+//                continue;
+//            }
             // left case
-            if(nums[mid] > nums[start]){
+            if(nums[mid] >= nums[start]){
                 // left side sorted
                 if(nums[start] > ans && ans < nums[mid]){
                     end = mid - 1;
@@ -33,7 +35,8 @@ public class Problem153 {
                     start = mid + 1;
                 }
             }
-            if(nums[mid] < nums[end]){
+
+            if(nums[mid] <= nums[end]){
                 // right side sorted
                 if(nums[mid] > ans && nums[end] < ans){
                     start = mid +1;
