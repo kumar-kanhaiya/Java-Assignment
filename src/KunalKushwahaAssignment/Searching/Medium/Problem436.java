d31babf84421b9e194980d6a5736a83d9d488bd3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-21 17:06:14 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem436.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem436.java
@@ -1,23 +1,38 @@
 package KunalKushwahaAssignment.Searching.Medium;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Problem436 {
     public static void main(String[] args) {
+        int[][] array = { {1,2},{2,3},{0,1}};
+        System.out.println(Arrays.toString(findRightInterval(array)));
 
     }
     public static int[] findRightInterval(int[][] intervals) {
         int[] ans = new int[intervals.length];
         Queue list = new LinkedList();
         for(int i = 0 ; i < intervals.length ; i++){
-            for (int j = i+1; j < intervals[i].length ; j++) {
-                if(j == intervals.length ){
-                    i = 0;
+            int check = i+1;
+
+            if(check == intervals.length ){
+                check = 0;
+            }
+            int fi = intervals[i][1];
+            for (int j = 0; j < intervals.length ; j++) {
+                if(intervals[check][j] >= fi ){
+                    list.add(intervals[i][0]);
                 }
-                else if(isGreater(intervals[i][j] , ))
+                else{
+                    list.add(-1);
+                }
             }
         }
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int) list.remove();
+        }
+        return ans;
 
     }
 
