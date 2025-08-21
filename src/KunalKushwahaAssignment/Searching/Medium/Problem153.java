35974c14b9f0726b0d2e085a7402d01cf4de9b0d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-21 15:52:16 +0530

    problem153

--- src/KunalKushwahaAssignment/Searching/Medium/Problem153.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem153.java
@@ -7,7 +7,43 @@ public class Problem153 {
 
     }
     public static int findMin(int[] nums) {
+        int start = 0;
+        int ans = nums[0];
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
 
+            if(nums[mid] < ans){
+                ans = nums[mid];
+            }
+            if(nums[start] == nums[mid] && nums[end] == nums[mid]){
+                // case of repeating number
+                start++;
+                end--;
+                continue;
+            }
+            // left case
+            if(nums[mid] > nums[start]){
+                // left side sorted
+                if(nums[start] > ans && ans < nums[mid]){
+                    end = mid - 1;
+
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            if(nums[mid] < nums[end]){
+                // right side sorted
+                if(nums[mid] > ans && nums[end] < ans){
+                    start = mid +1;
+                }
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+        return ans;
     }
 
 }
