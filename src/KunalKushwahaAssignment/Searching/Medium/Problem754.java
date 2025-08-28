7eb1d713a31f969cd01c1dd77375d3e52d1ff770
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-28 20:05:26 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem754.java
@@ -0,0 +1,10 @@
+package KunalKushwahaAssignment.Searching.Medium;
+
+public class Problem754 {
+    public static void main(String[] args) {
+
+    }
+    public int reachNumber(int target) {
+
+    }
+}
