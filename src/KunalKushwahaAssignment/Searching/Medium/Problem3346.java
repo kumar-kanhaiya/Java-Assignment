0142b6d075466387bd48d423b50dd01fe5a7df83
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-28 19:39:54 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem3346.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem3346.java
@@ -5,14 +5,33 @@ import java.util.Arrays;
 public class Problem3346 {
     public static void main(String[] args) {
         int[] array = {5,8,9,4,6,46,2,3};
-        sortInDecreasing(array);
+//        sortInDecreasing(array);
         System.out.println(Arrays.toString(array));
 
     }
 
-//    public static int[][] sortMatrix(int[][] grid) {
-//
-//    }
+    public static int[][] sortMatrix(int[][] grid) {
+        int srow = 0 ;
+        int scoll  = 0;
+        int[] demo = new int[grid.length];
+        // case for middle
+        int i = 0;
+        while(srow < grid.length && scoll < grid.length && i< grid.length){
+            demo[i] = grid[srow][scoll];
+            i++;
+            srow++;
+            scoll++;
+        }
+        sortInDecreasing(demo);
+        int j = 0;
+        while(srow < grid.length && scoll < grid.length && j< grid.length){
+            grid[srow][scoll] = demo[j];
+            j++;
+            srow++;
+            scoll++;
+        }
+        return grid;
+    }
 
     public static void sortInIncreasing(int[] array){
 
@@ -49,14 +68,6 @@ public class Problem3346 {
         }
         return max;
     }
-    public static int minIndex(int[] array , int start , int end){
-        int min = 0;
-        for (int i = start; i < end; i++) {
-            if(array[i] < array[min]){
-                min = i;
-            }
-        }
-        return min;
-    }
+
 
 }
--- src/KunalKushwahaAssignment/Searching/Medium/Problem436.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem436.java
@@ -12,33 +12,9 @@ public class Problem436 {
     }
     // this question is done with the help of binary search and sorting
     public static int[] findRightInterval(int[][] intervals) {
-//        int start = intervals.length;
-//        int end = intervals[0].length;
-
-        int first = 0 ;
-        int index = 0;
-        int[] ans = new int[intervals.length];
-        while(first <= intervals.length - 1) {
-            for (int i = 0; i < intervals.length; i++) {
-
-                if(i == first){
-                    continue;
-                }
-
-                if (intervals[i][0] >= intervals[first][1]) {
-                    ans[index] = i;
-                    index++;
-                }
-                if(!(intervals[i][0] >= intervals[first][1])){
-                    ans[index] = -1;
-                    index++;
-                }
-            }
-            first++;
-        }
 
 
-        return ans;
+
     }
 
 }
