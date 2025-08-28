4707891acdee73c9e0e31d07826d56f2548c0de0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-28 19:55:32 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem436.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem436.java
@@ -10,26 +10,39 @@ public class Problem436 {
     }
     // this question is done with the help of binary search and sorting
     public static int[] findRightInterval(int[][] intervals) {
-
-
+        int start = 0;
+        int end = intervals.length;
+        int[][] pairs = new int[end][2];
+        for (int i = 0; i < end; i++) {
+            pairs[i][0] = intervals[i][0];
+            pairs[i][1] = i;
+        }
+        int[] ans = new int[intervals.length];
+        int index = 0;
+        while(start < end){
+            ans[start] = binarySearch(list , intervals[start][1]);
+        }
+        return ans;
     }
 
-    public static int binarySearch(ArrayList<Integer> list , int target){
+    public static int binarySearch(int[][] pairs , int target){
         int start = 0;
-        int end = list.size();
-        Collections.sort(list);
+        int end = pairs.length - 1;
+        int ans = -1;
+
         while(start <= end){
             int mid = start + (end - start)/2;
-            if(list.get(mid) >= target){
+            if(pairs[mid][0] >= target){
                 // ans found
-                return mid;
+                ans = mid;
+                end = mid - 1;
             }
             else{
                 start = mid + 1;
             }
 
         }
-        return -1;
+        return ans;
     }
 
 }
