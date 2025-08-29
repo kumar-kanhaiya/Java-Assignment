b346e9c59886c5acb9987490425d05f6e7f44dc2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-29 17:47:48 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem754.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem754.java
@@ -2,9 +2,32 @@ package KunalKushwahaAssignment.Searching.Medium;
 
 public class Problem754 {
     public static void main(String[] args) {
+        System.out.println(reachNumber(3));
 
     }
-//    public int reachNumber(int target) {
-//
-//    }
+    public static int reachNumber(int target) {
+        // we solve this problem using binary search
+        if(target == 0){
+            return 0;
+        }
+        int i = 1 ;
+        int steps = 0;
+        int start = 0 ;
+        while(start != target){
+            if((start + i ) ==  target || (start - i) == target){
+                return steps;
+            }
+
+            if(start+i > target){
+                start = start - i;
+                i++;
+            }
+            else{
+                start = start + i;
+                i++;
+            }
+        }
+        return steps;
+
+    }
 }
