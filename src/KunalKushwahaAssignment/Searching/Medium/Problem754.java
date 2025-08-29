3f42fb33da644c4201bd4b1aa79e0f53abac4d48
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-29 18:14:23 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem754.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem754.java
@@ -1,33 +1,42 @@
-package KunalKushwahaAssignment.Searching.Medium;
+    package KunalKushwahaAssignment.Searching.Medium;
 
-public class Problem754 {
-    public static void main(String[] args) {
-        System.out.println(reachNumber(3));
+    public class Problem754 {
+        public static void main(String[] args) {
+            System.out.println(reachNumber(4));
 
-    }
-    public static int reachNumber(int target) {
-        // we solve this problem using binary search
-        if(target == 0){
-            return 0;
         }
-        int i = 1 ;
-        int steps = 0;
-        int start = 0 ;
-        while(start != target){
-            if((start + i ) ==  target || (start - i) == target){
-                return steps;
+        public static int reachNumber(int target) {
+            // we solve this problem using binary search
+            if(target == 0){
+                return 0;
             }
+            int i = 1 ;
+            int steps = 1;
+            int sum = 0 ;
+            while(sum >= target && (sum - target) % 2 == 0){
+//                sum = sum+i;
+//                if((sum ) ==  Math.abs(target) ){
+//                    return steps;
+//                }
 
-            if(start+i > target){
-                start = start - i;
-                i++;
+                if(sum +i > Math.abs(target)){
+                    if(sum >= target && (sum - target)%2 == 0 ){
+                        return steps;
+
+                    }
+                    else{
+                        sum += 1;
+                        steps++;
+                        i++;
+                    }
+                }
+                else{
+                    sum = sum + i;
+                    steps++;
+                    i++;
+                }
             }
-            else{
-                start = start + i;
-                i++;
-            }
+            return steps;
+
         }
-        return steps;
-
     }
-}
