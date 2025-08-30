94e9988c95e0120cda35f8002aef587c83858b4e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-30 19:05:49 +0530

    codE

---
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1802.java
@@ -0,0 +1,23 @@
+package KunalKushwahaAssignment.Searching.Medium;
+
+public class Problem1802 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxValue(int n, int index, int maxSum) {
+        int[] ans = new int[n];
+        while(sum(ans) <= maxSum){
+
+        }
+
+    }
+    public static int sum(int[] array ){
+        int sum = 0;
+        for(int element : array){
+            sum += element;
+        }
+        return sum;
+
+    }
+}
