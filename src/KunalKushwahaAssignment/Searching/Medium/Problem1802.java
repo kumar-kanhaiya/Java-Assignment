dc16028ed0bcc35a4358c90f9923cc605b2e19c4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-02 18:25:05 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem1802.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1802.java
@@ -61,5 +61,50 @@ public class Problem1802 {
 
         return sum <= maxSum;
     }
+    // 2nd approach
+    public static int maxValue2(int n, int index, int maxSum){
+//        long sum = 0;
+        long r = n - index - 1;
+        long l = index;
+        long end = maxSum;
+        long start = 1;
+        long res = 0;
 
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            long sum = mid;
+            long ls = 0; // sum on left side
+            long rs = 0; // sum on right side
+            long m = mid - 1;
+
+            if(r <= m){
+                rs = m*(m+1)/2 - (m - r)*(m -r + 1 )/2;
+            }
+            else{
+                // extra filling 1
+                rs = m*(m+1)/2 - 1*(r-m);
+
+            }
+
+            if(l <= m){
+                ls = m*(m+1)/2 - (m - l)*(m -l + 1 )/2;
+
+            }
+            else{
+                ls = m*(m+1)/2 - 1*(l-m);
+
+            }
+
+            sum += ls + rs;
+
+            if(sum<= maxSum){
+                res = mid;
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return (int)res;
+    }
 }
