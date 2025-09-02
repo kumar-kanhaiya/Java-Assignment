4f594789153e9ad1ced87d655caff08b813ffab5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-02 21:59:05 +0530

    code' git p

--- src/KunalKushwahaAssignment/Searching/Medium/Problem875.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem875.java
@@ -5,6 +5,34 @@ public class Problem875 {
 
     }
     public static int minEatingSpeed(int[] piles, int h) {
+//        int totalHour = 0;
+        int start = 1;
+        int end = maxElement(piles);
 
+        // we do using binary search
+        while (start <= end){
+            int mid = start  + (end - start)/2 ;
+            int totalHour = 0;
+            for (int pile : piles) {
+                totalHour += (pile + mid - 1) / mid;
+            }
+
+            if(totalHour <= h){
+                end = mid -1;
+            }
+            if(totalHour > h){
+                start = mid +1;
+            }
+        }
+        return start;
+    }
+    public static int maxElement(int[] array){
+        int check = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > check){
+                check = array[i];
+            }
+        }
+        return check;
     }
 }
