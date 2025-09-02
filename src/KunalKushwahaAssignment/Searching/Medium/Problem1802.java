7939c4a006288ce466506a5f3f284b68dce9b990
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-02 17:20:23 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem1802.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1802.java
@@ -2,50 +2,48 @@ package KunalKushwahaAssignment.Searching.Medium;
 
 public class Problem1802 {
     public static void main(String[] args) {
-        System.out.println(maxValue(4,2,6));
+        System.out.println(maxValue(6,1,10));
 
     }
 
     public static int maxValue(int n, int index, int maxSum) {
 
-        int sum = 0;
-        int fill = 1 ;
+        int low= 0;
+        int high = maxSum ;
+        int ans = 1;
 
-        while(sum  <= maxSum){
+        while(low <= high){
+            int mid = (low + high)/2;
 
-            // terminating case
-            if(sum == maxSum){
-                return fill;
+            if(canPlace(mid , n , index , maxSum)){
+                ans = mid;
+                low = mid+ 1;
             }
-            if(sum > maxSum){
-                return fill- 1;
+            else{
+                high = mid -1;
             }
-            sum = 0;
-            // middle the main case
-            sum += fill;
-
-
-            // for left case
-            int l = 1;
-            for (int i = 0; i < index; i++) {
-                sum += l;
-                l++;
-
-            }
-
-            // for right Case
-            int r = 1;
-            for (int i = index+ 1; i < n; i++) {
-                sum += r;
-                r++;
-
-            }
-            fill++;
 
         }
-        return -1;
 
+        return ans;
+    }
 
+    public static  boolean canPlace(int mid , int n , int index , int maxSum){
+        int sum = 0 ;
+        sum += mid;
+        // left case
+        int l = 1;
+        for (int i = 0; i < index ; i++) {
+            sum += l;
+            l++;
+        }
+        int r = 1;
+        for (int i = n ; i > index + 1; i--) {
+            sum += r;
+            r++;
+        }
+
+        return sum <= maxSum;
     }
 
 }
