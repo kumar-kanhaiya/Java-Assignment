87774ee4fb6c1fc6693378402ec5d9422fbd86a5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-05 11:29:45 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem74.java
@@ -0,0 +1,11 @@
+package KunalKushwahaAssignment.Searching.Medium;
+
+public class Problem74 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+    }
+}
