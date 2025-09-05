07c50716c5b195e94d4545ef476183d67b3b5277
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-05 12:32:16 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem74.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem74.java
@@ -6,6 +6,30 @@ public class Problem74 {
     }
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int rEnd = matrix.length;
+        int coll = 0;
+        int cEnd = matrix[0].length;
+        while(row < rEnd && coll < cEnd){
+            int mid = coll + (cEnd - coll) / 2;
 
+
+            // checking the row
+            if(matrix[row][0] > target){
+                // search below the row ;
+                if(matrix[row-1][mid] == target){
+                    return true;
+                }
+                else if(matrix[row-1][mid] > target){
+                    cEnd = mid - 1;
+                }
+                else{
+                    coll = mid + 1;
+                }
+            }
+            row++;
+            coll = 0;
+        }
+        return false;
     }
 }
