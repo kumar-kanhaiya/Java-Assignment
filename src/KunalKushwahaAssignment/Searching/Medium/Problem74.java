4daf55e995857af28e1585130bc52802111c1967
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-05 12:39:02 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem74.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem74.java
@@ -34,5 +34,18 @@ public class Problem74 {
         // use the binary search to search the element
         int left = 0;
         int right = coll - 1;
+        while(left<= right){
+            int mid = left + (right - left)/2;
+            if(matrix[row][mid] == target){
+                return true;
+            }
+            else if(matrix[row][mid] > target){
+                left = mid + 1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return false;
     }
 }
