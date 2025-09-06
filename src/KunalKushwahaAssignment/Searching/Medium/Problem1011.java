e1444f8a79ee5f5310238df557bb1deef25efdba
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-06 18:22:48 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1011.java
@@ -0,0 +1,12 @@
+package KunalKushwahaAssignment.Searching.Medium;
+
+public class Problem1011 {
+    public static void main(String[] args) {
+
+    }
+    // array is already sorted
+    // so we use binary search for this problem also it is done with the help of sliding window
+    public int shipWithinDays(int[] weights, int days) {
+
+    }
+}
