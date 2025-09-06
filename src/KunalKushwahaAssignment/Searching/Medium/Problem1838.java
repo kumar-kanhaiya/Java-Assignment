bc3e86ffb406d1a1db93a6d36919f1a7b271b16a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-06 17:52:51 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem1838.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1838.java
@@ -7,9 +7,11 @@ public class Problem1838 {
         int[] arr= {1,2,5,6,8,9,4,5,63,0};
         sorting(arr);
         System.out.println(Arrays.toString(arr));
+        int[] array = {1,2,4};
+        System.out.println(maxFrequency(array,5));
 
     }
-    public int maxFrequency(int[] arr, int k) {
+    public static int maxFrequency(int[] arr, int k) {
         // this problem is done with the help of sliding window
         sorting(arr);
 //        Arrays.sort(arr);
@@ -20,13 +22,13 @@ public class Problem1838 {
         for (int r = 0; r < arr.length; r++) {
             sum += arr[r];
 
-            // calculate cost: make all numbers in [l..r] equal to arr[r]
-            while ((long) arr[r] * (r - l + 1) - sum > k) {
+            int windowSize = (r- l + 1);
+            while ((long) arr[r] * windowSize - sum > k) {
                 sum -= arr[l];  // shrink from left
                 l++;
             }
 
-            result = Math.max(result, r - l + 1);
+            result = Math.max(result, windowSize);
         }
 
         return result;
