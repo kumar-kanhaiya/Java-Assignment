953f139ba128c20701b98bd8af2932e9d74c6452
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-06 19:55:47 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem1011.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1011.java
@@ -8,39 +8,52 @@ public class Problem1011 {
     }
     // array is already sorted
     // so we use binary search for this problem also it is done with the help of sliding window
-    public int shipWithinDays(int[] weights, int days) {
-//        Arrays.sort(weights);
-//        int day = 0;
-//        int load = 0;
-//        for (int i = 0; i < weights.length; i++) {
-//            if(load + weights[i] > capacity){
-//                day = day + 1;
-//                load = weights[i];
-//            }
-//            else{
-//                load += weights[i];
-//            }
-//        }
-//        return day;
-        Arrays.sort(weights);
-        int start = max(weights);
-        int end = 0;
-        for (int i = 0; i < weights.length ; i++) {
-            end += weights[i];
-        }
+        public int shipWithinDays(int[] weights, int days) {
+    //        Arrays.sort(weights);
 
-        while(start <= end){
-
-        }
-
-    }
-    public static int max(int[] array ){
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] > max){
-                max = array[i];
+    //        Arrays.sort(weights);
+            int start = max(weights);
+            int end = 0;
+            for (int i = 0; i < weights.length ; i++) {
+                end += weights[i];
             }
+
+            while(start <= end){
+                int mid = start + (end - start)/2;
+                int noOfDays = noOfDays(weights , mid);
+                if(noOfDays <= days){
+                    end = mid - 1 ;
+                }
+                else{
+                    start = mid +1;
+                }
+
+            }
+            return start;
+
         }
-        return max;
-    }
+
+        public static int noOfDays(int[] weights , int capacity){
+            int day = 0;
+            int load = 0;
+            for (int i = 0; i < weights.length; i++) {
+                if(load + weights[i] > capacity){
+                    day = day + 1;
+                    load = weights[i];
+                }
+                else{
+                    load += weights[i];
+                }
+            }
+            return day;
+        }
+        public static int max(int[] array ){
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < array.length; i++) {
+                if(array[i] > max){
+                    max = array[i];
+                }
+            }
+            return max;
+        }
 }
