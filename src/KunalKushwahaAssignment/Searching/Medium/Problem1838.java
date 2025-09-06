40bf1547ff82dd6f5bbd4df4416024c82f4a5675
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-06 12:59:38 +0530

    code

---
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem1838.java
@@ -0,0 +1,11 @@
+package KunalKushwahaAssignment.Searching.Medium;
+
+public class Problem1838 {
+    public static void main(String[] args) {
+
+    }
+    public int maxFrequency(int[] nums, int k) {
+
+
+    }
+}
