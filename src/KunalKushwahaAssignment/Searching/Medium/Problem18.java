2a3d6dbeb54de91d8b04db62d3a80120acb69f44
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-08 09:43:47 +0530

    code

--- src/KunalKushwahaAssignment/Searching/Medium/Problem18.java
+++ src/KunalKushwahaAssignment/Searching/Medium/Problem18.java
@@ -1,12 +1,39 @@
 package KunalKushwahaAssignment.Searching.Medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem18 {
     public static void main(String[] args) {
 
     }
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    public List<List<Integer>> fourSum(int[] array, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
 
+        // best case
+        if(array.length == 4) {
+            if (array[0] + array[1] + array[2] + array[3] != target) {
+                return ans;
+            }
+        }
+
+
+
+    }
+
+    public static List<Integer> helper(int[] array , int start , int end , List<Integer> list , int target){
+        if(start == end){
+            return list;
+        }
+        // we are doing using two pointer approach
+        for (int i = 0; i <end ; i++) {
+            int a = 0;
+            for (int j = 0; j < end; j++) {
+                if(i == j){
+                    continue;
+                }
+                if( a + array[j]  <= target)
+            }
+        }
     }
 }
