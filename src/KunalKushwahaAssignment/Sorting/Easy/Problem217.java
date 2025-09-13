c12a70d9abff3e026bee379072b3a72684811c9d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-13 11:19:57 +0530

    code

---
+++ src/KunalKushwahaAssignment/Sorting/Easy/Problem217.java
@@ -0,0 +1,66 @@
+package KunalKushwahaAssignment.Sorting.Easy;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+
+public class Problem217 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,1};
+        System.out.println(containsDuplicate(array));
+
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        // approach first sort the array then check duplicate element
+        mergeSort(nums , 0 , nums.length - 1);
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] == nums[i]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static void mergeSort(int[] array , int low , int high ){
+        int mid = (low + high)/2;
+
+        if(low >= high){
+            return;
+        }
+
+        mergeSort(array , low , mid);
+        mergeSort(array , mid + 1 , high);
+        merge(array, low , mid , high);
+    }
+    public static void merge(int[] array , int low , int mid , int high){
+        Queue<Integer> temp = new ArrayDeque<>();
+        int left = low;
+        int right = mid + 1;
+
+
+        while(left<= mid && right<= high){
+            if(array[left] < array[right]){
+                temp.add(array[left]);
+                left++;
+            }
+            else {
+                temp.add(array[right]);
+                right++;
+            }
+        }
+        while(left <= mid){
+            temp.add(array[left]);
+            left++;
+        }
+        while(right<= high){
+            temp.add(array[right]);
+            right++;
+        }
+        // insert the element in new array
+        for (int i = low; i <= high; i++) {
+            array[i] = temp.remove();
+        }
+    }
+
+}
