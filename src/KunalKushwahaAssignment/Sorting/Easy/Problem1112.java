5f75424c445ae1dba94ef4b2ef2def97d08a800d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-17 19:42:58 +0530

    code

--- src/KunalKushwahaAssignment/Sorting/Easy/Problem1112.java
+++ src/KunalKushwahaAssignment/Sorting/Easy/Problem1112.java
@@ -1,10 +1,22 @@
 package KunalKushwahaAssignment.Sorting.Easy;
 
+import java.util.Arrays;
+
 public class Problem1112 {
     public static void main(String[] args) {
 
     }
     public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length];
+        // sort the arr1
+        Arrays.sort(arr1);
+        // sort according the arr2 with using swap method
 
+
+    }
+    public static void swap(int[] array , int first , int second){
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
     }
 }
