248bf814ba0052d394e989494e7098f311530c2c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-18 16:09:40 +0530

    code

--- src/KunalKushwahaAssignment/Sorting/Easy/Problem1200.java
+++ src/KunalKushwahaAssignment/Sorting/Easy/Problem1200.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class Problem1200 {
     public static void main(String[] args) {
+        int[] arr = {4,2,1,3};
+        System.out.println(minimumAbsDifference(arr));
 
     }
 
@@ -13,13 +15,23 @@ public class Problem1200 {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(arr);
         int min = minDifference(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i+1] - arr[i] == min) {
+
+                List<Integer> check = new ArrayList<>();
+                check.add(arr[i]);
+                check.add(arr[i+1]);
+                ans.add(check);
+            }
+        }
+        return ans;
 
 
     }
     public static int minDifference(int[] array){
         int min = Integer.MAX_VALUE;
-        for (int i = 1; i < array.length ; i++) {
-            min = Math.min(min , array[i] - array[i+1]);
+        for (int i = 1; i < array.length -1; i++) {
+            min = Math.min(min ,  array[i+1]-array[i] );
         }
         return min;
     }
