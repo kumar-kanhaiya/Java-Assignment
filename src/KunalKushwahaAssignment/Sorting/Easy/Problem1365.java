256a866cdf734327e395cc4c7f1a3691499f2a7c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-19 11:42:36 +0530

    code

---
+++ src/KunalKushwahaAssignment/Sorting/Easy/Problem1365.java
@@ -0,0 +1,24 @@
+package KunalKushwahaAssignment.Sorting.Easy;
+
+import java.util.Arrays;
+
+public class Problem1365 {
+    public static void main(String[] args) {
+        int[] array = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(array)));
+        
+    }
+    public static  int[] smallerNumbersThanCurrent(int[] nums){
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length ; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length ; j++) {
+                if(nums[i] > nums[j] ){
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
