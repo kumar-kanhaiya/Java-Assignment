4acd84a6262c36a3cc7baa34c21629279a348345
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-19 18:24:01 +0530

    code

---
+++ src/KunalKushwahaAssignment/Sorting/Easy/Problem1491.java
@@ -0,0 +1,22 @@
+package KunalKushwahaAssignment.Sorting.Easy;
+
+import java.util.Arrays;
+
+public class Problem1491 {
+    public static void main(String[] args) {
+        int[] array = {1000,2000,3000};
+        System.out.println(average(array));
+
+    }
+    public static double average(int[] salary) {
+        Arrays.sort(salary);
+        if(salary.length <2){
+            return 0;
+        }
+        double sum = 0;
+        for (int i = 1; i < salary.length-1 ; i++) {
+            sum += salary[i];
+        }
+        return (double)sum/(salary.length -2);
+    }
+}
