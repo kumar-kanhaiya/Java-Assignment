bc172f3cb6ce856bcbd313c627b53499f7b557c5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-20 11:34:08 +0530

    code

---
+++ src/KunalKushwahaAssignment/Sorting/Easy/Problem1636.java
@@ -0,0 +1,32 @@
+package KunalKushwahaAssignment.Sorting.Easy;
+
+import java.util.Arrays;
+
+public class Problem1636 {
+    public static void main(String[] args) {
+
+    }
+    public static int[] frequencySort(int[] nums) {
+
+
+    }
+    public static int occurence(int[] array , int target ){
+        int count = 0;
+        for (int i = 0; i < array.length ; i++) {
+            if(array[i] == target){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void increasingSort(int[] array){
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
+    public static void swap(int[] array , int first , int second){
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+}
