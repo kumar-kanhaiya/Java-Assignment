8e9d381fe5c6edce6331e768df067de6daf88d15
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-21 12:18:43 +0530

    code

--- src/KunalKushwahaAssignment/Sorting/Medium/Problem16.java
+++ src/KunalKushwahaAssignment/Sorting/Medium/Problem16.java
@@ -4,32 +4,47 @@ import java.util.Arrays;
 
 public class Problem16 {
     public static void main(String[] args) {
-        int[] array = {-1,2,1,-4};
+        int[] array = {10,20,30,40,50,60,70,80,90};
         System.out.println(threeSumClosest(array , 1));
 
     }
 
     public static int threeSumClosest(int[] array, int target) {
         Arrays.sort(array);
-        int ans = 0;
+        int left = 0;
+        int right = sum(array) ;
         for (int i = 0; i < array.length ; i++) {
             int j = i+1;
             int k = array.length-1;
             while(j< k){
                 int check = array[i] + array[j] + array[k];
                 if(check <= target){
-                    ans = Math.min(ans,check);
+                    left = Math.max(left,check);
                     j++;
                     k--;
                 }
                 else {
-                    ans  = Math.max(ans,check);
+                    right  = Math.min(right,check);
                     j++;
                 }
 
 
             }
         }
-        return ans;
+        return close(left, right, target);
+
+    }
+    public static int sum(int[] array){
+        int sum = 0;
+        for(int element : array){
+            sum += element;
+        }
+        return sum;
+    }
+    public static int close(int left , int right , int target){
+        if(left > 0  && right > 0 && Math.abs(left - target) < Math.abs(right - target)){
+            return left;
+        }
+        return right;
     }
 }
