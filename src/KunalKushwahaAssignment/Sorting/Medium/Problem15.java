c97f6269e1d66af6cada7e659ef5538d3d874fc7
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-21 11:20:01 +0530

    codE

--- src/KunalKushwahaAssignment/Sorting/Medium/Problem15.java
+++ src/KunalKushwahaAssignment/Sorting/Medium/Problem15.java
@@ -1,12 +1,34 @@
 package KunalKushwahaAssignment.Sorting.Medium;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem15 {
     public static void main(String[] args) {
+        int[] array = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(array));
 
     }
-    public static List<List<Integer>> threeSum(int[] nums) {
-
+    public static List<List<Integer>> threeSum(int[] arr) {
+        // normal approach
+        List<List<Integer>> ans = new ArrayList<>();
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = i+1; j < arr.length ; j++) {
+                int k = j+1;
+                while(k< arr.length){
+                    if(arr[k] + arr[j] == -(arr[i]) ){
+                        List<Integer> check = new ArrayList<>();
+                        check.add(arr[i]);
+                        check.add(arr[j]);
+                        check.add(arr[k]);
+                        set.add(check);
+                    }
+                    k++;
+                }
+            }
+        }
+        ans.addAll(set);
+        return ans;
     }
 }
