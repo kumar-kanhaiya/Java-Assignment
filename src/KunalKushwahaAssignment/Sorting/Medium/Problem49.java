49eb9e54cb5e7f0a613f5991b0c214e496ed61f4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-22 18:56:13 +0530

    code

--- src/KunalKushwahaAssignment/Sorting/Medium/Problem49.java
+++ src/KunalKushwahaAssignment/Sorting/Medium/Problem49.java
@@ -1,22 +1,34 @@
 package KunalKushwahaAssignment.Sorting.Medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Problem49 {
     public static void main(String[] args) {
         String check = "tea";
         System.out.println(sorted(check));
+        String[] str = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(str));
 
     }
 
-    public List<List<String>> groupAnagrams(String[] str) {
-        List<List<String>> ans = new ArrayList<>();
-        return ans;
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            String key = sorted(s);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
     }
 
     public static String sorted(String str){
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+    public static String sorted2(String str){
         int[] arr = new int[str.length()];
         for (int i = 0; i < arr.length ; i++) {
             arr[i] = (int) str.charAt(i);
