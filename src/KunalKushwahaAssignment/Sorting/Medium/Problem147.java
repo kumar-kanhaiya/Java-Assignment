9d7f4db3ad32d8885933792456d3996f91cd6566
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-24 08:55:00 +0530

    code

---
+++ src/KunalKushwahaAssignment/Sorting/Medium/Problem147.java
@@ -0,0 +1,25 @@
+package KunalKushwahaAssignment.Sorting.Medium;
+
+public class Problem147 {
+    public static void main(String[] args) {
+
+    }
+
+//      Definition for singly-linked list.
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) {
+              this.val = val;
+          }
+          ListNode(int val, ListNode next) {
+              this.val = val; this.next = next;
+          }
+      }
+
+    public static ListNode insertionSortList(ListNode head) {
+
+    }
+
+}
