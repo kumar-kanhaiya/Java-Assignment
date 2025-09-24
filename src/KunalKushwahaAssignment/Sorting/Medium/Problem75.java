99e4786db357a938022865b994d1e1c2bf7b6c0a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-24 08:50:33 +0530

    code

---
+++ src/KunalKushwahaAssignment/Sorting/Medium/Problem75.java
@@ -0,0 +1,35 @@
+package KunalKushwahaAssignment.Sorting.Medium;
+
+import java.util.Arrays;
+
+public class Problem75 {
+    public static void main(String[] args) {
+        int[] array = {2,0,2,1,1,0};
+        sortColors(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    public static  void sortColors(int[] nums) {
+        // this is done by sorting we use selection sort for this question
+        for (int i = 0; i < nums.length; i++) {
+            int last = nums.length - i - 1;
+            int maxIndex = maxIndex(nums , 0 , last);
+            swap(nums,maxIndex,last);
+        }
+    }
+    public static int maxIndex(int[] array , int start , int end ){
+        int maxIn = start;
+        for (int i = 0; i <= end ; i++) {
+            if(array[maxIn]< array[i]){
+                maxIn = i;
+            }
+        }
+        return maxIn;
+    }
+    public static void swap(int[] array , int first , int second ){
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+}
