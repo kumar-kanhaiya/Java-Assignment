c98cbb4719fd147191677ba08d0e82fae9eaaaff
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-27 17:30:21 +0530

    codE

---
+++ src/KunalKushwahaAssignment/Sorting/Hard/Problem41.java
@@ -0,0 +1,28 @@
+package KunalKushwahaAssignment.Sorting.Hard;
+
+public class Problem41 {
+    public static void main(String[] args) {
+
+    }
+    public  int firstMissingPositive(int[] array) {
+        int i = 0 ;
+        while(i < array.length){
+            int correctIndex = array[i] - 1;
+
+            if(array[i] > 0 && array[i]<=array.length && array[i] != array[correctIndex]){
+                swap(array , i , correctIndex);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int j = 0 ; j < array.length ; j++){
+            if(array[j] != j + 1){
+                return j+1;
+            }
+        }
+        return array.length + 1;
+    }
+
+}
