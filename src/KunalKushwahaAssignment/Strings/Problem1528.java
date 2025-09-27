4e92aeb8d73dd8c866b5335d4a075d51e7877cd9
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-27 18:13:38 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Problem1528.java
+++ src/KunalKushwahaAssignment/Strings/Problem1528.java
@@ -1,5 +1,7 @@
 package KunalKushwahaAssignment.Strings;
 
+import java.util.*;
+
 public class Problem1528 {
     public static void main(String[] args) {
         String s = "codeleet";
@@ -8,11 +10,34 @@ public class Problem1528 {
 
     }
 
-    public static String restoreString(String s, int[] indices) {
+    public static String restoreString2(String s, int[] indices) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < indices.length ; i++) {
             stringBuilder.append(s.charAt(indices[i]));
         }
         return stringBuilder.toString();
     }
+
+    public static String restoreString(String s , int[] indices){
+        Map<Integer , Character> map = new HashMap<>();
+        for (int i = 0; i < indices.length; i++) {
+            map.put(indices[i] , s.charAt(i));
+        }
+        ArrayList<Integer> sortedKeys = new ArrayList<>(map.keySet());
+        Collections.sort(sortedKeys);
+
+        String ans = "";
+        for(int element : sortedKeys){
+            ans += map.get(element);
+        }
+        return ans;
+    }
+    public String restoreString3(String s, int[] indices) {
+        char[] res = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            res[indices[i]] = s.charAt(i);
+        }
+        return new String(res);
+    }
+
 }
