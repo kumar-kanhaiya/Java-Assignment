6bec0f2c1f212fab34cf1bac28dda7b06df0dff5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-29 09:13:50 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Easy/Problem28.java
+++ src/KunalKushwahaAssignment/Strings/Easy/Problem28.java
@@ -33,5 +33,26 @@ public class Problem28 {
         return -1;
     }
 
+    // 2nd approach
 
+    public static int strStr2(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+
+        if (m == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
