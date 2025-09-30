9953749434888348d223648f1ac4c2597ada18b5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-30 12:41:06 +0530

    code

---
+++ src/KunalKushwahaAssignment/Strings/Easy/Problem680.java
@@ -0,0 +1,17 @@
+package KunalKushwahaAssignment.Strings.Easy;
+
+public class Problem680 {
+    public static void main(String[] args) {
+
+    }
+
+//    public static boolean validPalindrome(String s) {
+//        StringBuilder check = new StringBuilder();
+//        check.append(s);
+//        String reverse = check.reverse().toString();
+//
+//
+//    }
+
+
+}
---
+++ src/KunalKushwahaAssignment/Strings/Easy/Problem925.java
@@ -0,0 +1,30 @@
+package KunalKushwahaAssignment.Strings.Easy;
+
+public class Problem925 {
+    public static void main(String[] args) {
+        String name = "alex";
+        String typed = "aaleex";
+
+    }
+
+    public static boolean isLongPressedName(String name, String typed) {
+        for (int i = 0; i < name.length() ; i++) {
+            if(occurence(typed,typed.charAt(i)) >= occurence(name,name.charAt(i))){
+
+            }
+        }
+
+    }
+
+    public static int occurence(String name , char target){
+        int ans = 0;
+        for (int i = 0; i < name.length() ; i++) {
+            if(name.charAt(i) == target){
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+
+}
