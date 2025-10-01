7e112aaadfbf9f7b7d606d3a181305e3b69e1590
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-01 11:35:19 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Easy/Problem13.java
+++ src/KunalKushwahaAssignment/Strings/Easy/Problem13.java
@@ -12,19 +12,49 @@ public class Problem13 {
         for (int i = 0; i < s.length(); i++) {
             char check = s.charAt(i);
             if(check == 'I'){
-                ans += 1;
+                if(i+1 < s.length() && s.charAt(i+1) == 'V' ){
+                    ans += 4;
+                    i = i+1;
+                }
+                else if(i+1 < s.length() && s.charAt(i+1) == 'X'){
+                    ans += 9;
+                    i += 1;
+                }
+                else {
+                    ans += 1;
+                }
             }
             else if(check == 'V'){
                 ans += 5 ;
             }
             else if(check == 'X'){
-                ans += 10;
+                if(i+1 < s.length() && s.charAt(i+1) == 'L' ){
+                    ans += 40;
+                    i = i+1;
+                }
+                else if(i+1 < s.length() && s.charAt(i+1) == 'C'){
+                    ans += 90;
+                    i += 1;
+                }
+                else {
+                    ans += 10;
+                }
             }
             else if(check == 'L'){
                 ans += 50;
             }
             else if(check == 'C'){
-                ans += 100;
+                if(i+1 < s.length() && s.charAt(i+1) == 'D' ){
+                    ans += 400;
+                    i = i+1;
+                }
+                else if(i+1 < s.length() && s.charAt(i+1) == 'M'){
+                    ans += 900;
+                    i += 1;
+                }
+                else {
+                    ans += 100;
+                }
             }
             else if(check == 'D'){
                 ans += 500;
