caf5e92d86149ba13ddab57b67234b0fed808bf0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-03 09:41:32 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Medium/Problem1616.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem1616.java
@@ -8,11 +8,26 @@ public class Problem1616 {
     }
 
     public static boolean checkPalindromeFormation(String a, String b) {
+        if(a.length() == 1 && b.length() == 1){
+            return true;
+        }
         String aSuffix = "";
         String bSuffix = "";
-        String apreffix = "";
-        String bpreffix = "";
+        String aPrefix = "";
+        String bPrefix = "";
 
+        int index = Math.max(findIndex(a,b,true) , findIndex(a,b,false));
+
+        for (int i = 0; i < index; i++) {
+            aPrefix += a.charAt(i);
+            bPrefix += b.charAt(i);
+        }
+        for (int i = index; i <a.length() ; i++) {
+            aSuffix += a.charAt(i);
+            bSuffix += b.charAt(i);
+        }
+
+        return (isPalindrome(aPrefix+bSuffix) || isPalindrome(bPrefix + aSuffix));
 
     }
 
@@ -24,22 +39,27 @@ public class Problem1616 {
         return rev.equals(str);
     }
 
-    public static int findIndex(String a , String b ){
-        int i = 0;
-        int j = b.length()-1;
-        while(i<a.length()){
-            char ch = a.charAt(i);
-            while(j>=0){
-                if(b.charAt(j) == ch){
-                    i++;
-                    j--;
+    public static int findIndex(String a , String b , boolean check ){
+        if(check){
+            for (int i = 0; i < a.length() ; i++) {
+                if(a.charAt(i) == b.charAt(b.length() - i -1)){
+                    continue;
                 }
                 else{
                     return i;
-                    break;
                 }
             }
-            i++;
         }
+        else{
+            for (int i = 0; i < b.length(); i++) {
+                if(b.charAt(i) == a.charAt(a.length() - i - 1)){
+                    continue;
+                }
+                else{
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
