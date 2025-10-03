9318e518ac9bd2e676c88f8902a87d7cb3913854
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-03 09:28:59 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Medium/Problem1616.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem1616.java
@@ -2,10 +2,44 @@ package KunalKushwahaAssignment.Strings.Medium;
 
 public class Problem1616 {
     public static void main(String[] args) {
+        String str = "ulaalu";
+        System.out.println(isPalindrome(str));
 
     }
 
     public static boolean checkPalindromeFormation(String a, String b) {
+        String aSuffix = "";
+        String bSuffix = "";
+        String apreffix = "";
+        String bpreffix = "";
 
+
+    }
+
+    public static boolean isPalindrome(String str){
+        String rev = "";
+        for (int i = 0; i < str.length() ;i++) {
+            rev = str.charAt(i) + rev;
+        }
+        return rev.equals(str);
+    }
+
+    public static int findIndex(String a , String b ){
+        int i = 0;
+        int j = b.length()-1;
+        while(i<a.length()){
+            char ch = a.charAt(i);
+            while(j>=0){
+                if(b.charAt(j) == ch){
+                    i++;
+                    j--;
+                }
+                else{
+                    return i;
+                    break;
+                }
+            }
+            i++;
+        }
     }
 }
