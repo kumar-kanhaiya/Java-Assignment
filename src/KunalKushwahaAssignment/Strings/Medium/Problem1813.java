4dc16252caa0b585148962872e51ccd8c1c94d60
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-04 13:32:59 +0530

    final code

--- src/KunalKushwahaAssignment/Strings/Medium/Problem1813.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem1813.java
@@ -1,53 +1,47 @@
-package KunalKushwahaAssignment.Strings.Medium;
+    package KunalKushwahaAssignment.Strings.Medium;
 
-public class Problem1813 {
-    public static void main(String[] args) {
-        String sentence1 =  "Eating right now";
-        String sentence2 = "Eating";
-        System.out.println(areSentencesSimilar(sentence1,sentence2));
+    public class Problem1813 {
+        public static void main(String[] args) {
+            String sentence1 =  "Eating right now";
+            String sentence2 = "Eating";
+            System.out.println(areSentencesSimilar(sentence1,sentence2));
 
-    }
-
-    public static boolean areSentencesSimilar(String sentence1, String sentence2) {
-        String small = "";
-        String large = "";
-
-        if(smallest(sentence1,sentence2)){
-            small = sentence2;
-            large = sentence1;
         }
-        else{
-            small = sentence1;
-            large = sentence2;
-        }
-        String[] word = large.split(" ");
-        String[] word2 = small.split(" ");
 
-        int start = -1 ;
-        int end = 0;
-        for (int i = 0; i < word.length ; i++) {
-            if(word2[0].equals(word[i])){
-                start = i;
+        public static boolean areSentencesSimilar(String sentence1, String sentence2) {
+            String small = "";
+            String large = "";
+
+            if(smallest(sentence1,sentence2)){
+                small = sentence2;
+                large = sentence1;
             }
-            if(word2[word2.length-1].equals(word[i]) && word2.length>1){
-                end = i;
+            else{
+                small = sentence1;
+                large = sentence2;
             }
-        }
-        if(start != 0 && end == 0){
-            return true;
-        }
+            String[] word1 = large.split(" ");
+            String[] word2 = small.split(" ");
 
-        if(end == 0){
+            int i = 0;
+            int j = 0;
+            while(i < word2.length && word1[i].equals(word2[i])){
+                i++;
+            }
+            while(j < word2.length && j < word1.length &&
+                    word1[word1.length - 1 - j].equals(word2[word2.length - 1 - j])){
+                j++;
+            }
+
+            if(i + j >= word2.length){
+                return true;
+            }
             return false;
+
         }
 
-
-        return true;
+        public static boolean smallest(String s , String p){
+            return s.length()>p.length() ? true:false;
+        }
 
     }
-
-    public static boolean smallest(String s , String p){
-        return s.length()>p.length() ? true:false;
-    }
-
-}
