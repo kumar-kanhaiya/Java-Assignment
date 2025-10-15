c4972e5dbc11d69e1dbcfb14cc4a30ff2eacddca
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-15 20:52:41 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Medium/Problem1573.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem1573.java
@@ -2,16 +2,24 @@ package KunalKushwahaAssignment.Strings.Medium;
 
 public class Problem1573 {
     public static void main(String[] args) {
+        String s = "10101";
 
     }
 
     public static int numWays(String s) {
-        int ans = 0;
-
-        for (int i = 0; i <s.length() ; i++) {
-
+        int totalOnes = repeatingOne(s);
+        if(totalOnes%3 != 0){
+            return 0;
         }
 
+    }
+    public static int repeatingOne(String s){
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '1'){
+                ans++;
+            }
+        }
         return ans;
     }
 }
