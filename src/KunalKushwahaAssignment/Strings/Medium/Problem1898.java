c0fccf2f1da77ec26ae88ecb09b10f20b4ca4946
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-21 22:14:34 +0530

    cdoe

--- src/KunalKushwahaAssignment/Strings/Medium/Problem1898.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem1898.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Problem1898 {
     public static void main(String[] args) {
-        String s = "abcbddddd";
-        String p = "abcd";
-        int[] removal = {3,2,1,4,5,6};
-        System.out.println(maximumRemovals(s,p,removal));
+        String s = "abcab";
+        String p = "abc";
+        int[] removal = {0,1,2,3,4};
+        System.out.println(maximumRemovals2(s,p,removal));
 
     }
 
@@ -37,5 +37,56 @@ public class Problem1898 {
             list.add(s.charAt(i));
         }
         return list;
+    }
+    
+    //2nd approach
+
+    //by using binary search for optimization
+    public static int maximumRemovals2(String s , String p , int[] removable){
+        int ans = 0;
+
+//        for (int i = 0; i < removable.length ; i++) {
+//            check[removable[i]] = true;
+//            if(!isSubsequence(s , p ,check)){
+//                return ans;
+//            }
+//            ans++;
+//        }
+//        return ans;
+        int start = 0;
+        int end = removable.length-1;
+        while(start<= end){
+            int mid = (start + end)/2;
+            boolean[] check = new boolean[s.length()];
+            for (int i = 0 ; i<= mid ; i++){
+                 check[removable[i]] = true;
+            }
+
+            if(!isSubsequence(s,p,check)){
+                end = mid - 1;
+            }
+            else{
+                ans = mid+1;
+
+                start = mid +1;
+
+
+            }
+
+
+        }
+        return ans;
+    }
+
+    public static boolean isSubsequence(String s , String p , boolean[] check){
+        int i = 0;
+        int j = 0;
+        while(i<s.length() && j < p.length()){
+            if(!check[i] && s.charAt(i) == p.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        return j == p.length();
     }
 }
