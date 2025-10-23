c96c8e2ae2e289cfa39a0b408995e78e87a30e00
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-23 18:40:21 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Medium/Problem43.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem43.java
@@ -9,15 +9,49 @@ public class Problem43 {
 
     }
 
-    public static String multiply(String num1, String num2) {
-        long result = (Integer.parseInt(num1) * Integer.parseInt(num2));
-        StringBuilder builder = new StringBuilder();
-//        while(result >=0){
-//            builder.append(result%10 + '0');
-//            result /=10;
-//        }
-        builder.append(String.valueOf(result));
-        return builder.toString();
+    // work for small value
+    public static String multiply2(String num1, String num2) {
 
+        long ans = 0;
+        long second = Long.parseLong(num2);
+        int placeValue = 1 ;
+        while(second>0){
+            ans += (Integer.parseInt(num1) * (second % 10))
+                    * placeValue ;
+            placeValue *=10;
+            second /=10;
+        }
+
+        return String.valueOf(ans);
+
+    }
+
+    // second approach for long value
+    public static String multiply(String num1 , String num2){
+        // condition check
+        if(num1.equals("0") && num2.equals("0")){
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+        int[] ans = new int[m+n];
+
+        // loop
+        for (int i = m-1; i >=0 ; i--) {
+            for (int j = n-1; j >=0 ; j--) {
+                int multiply = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = multiply + ans[i+j+1]; // use for carry sum
+                ans[i+j+1] = sum%10; // this is the main multiply
+                ans[i+j] += sum/10; // basically this is the carry
+            }
+        }
+        StringBuilder finalAns = new StringBuilder();
+        for(int digit : ans){
+            if(!(finalAns.length() == 0 && digit == 0)){
+                finalAns.append(digit);
+            }
+        }
+        return finalAns.length() == 0 ? "0" : finalAns.toString();
     }
 }
---
+++ src/KunalKushwahaAssignment/Strings/Problem3461.java
@@ -0,0 +1,7 @@
+package KunalKushwahaAssignment.Strings;
+
+public class Problem3461 {
+    public static void main(String[] args) {
+
+    }
+}
