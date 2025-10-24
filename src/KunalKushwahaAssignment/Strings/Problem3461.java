98186719c8ac1e9be512d53d4f102f9326612a10
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-24 09:12:41 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Problem3461.java
+++ src/KunalKushwahaAssignment/Strings/Problem3461.java
@@ -2,6 +2,30 @@ package KunalKushwahaAssignment.Strings;
 
 public class Problem3461 {
     public static void main(String[] args) {
+        String s = "3902";
+        System.out.println(hasSameDigits(s));
 
+    }
+
+    public static boolean hasSameDigits(String s) {
+        if(s.length() == 2){
+            if(s.charAt(0) == s.charAt(1)){
+                return true;
+            }
+            return false;
+        }
+
+        while(s.length() > 2){
+            StringBuilder newStr = new StringBuilder();
+            for (int i = 0; i < s.length()-1 ; i++) {
+                int digit1 = s.charAt(i) - '0';
+                int digit2 = s.charAt(i+1) - '0';
+                int sum = (digit1+digit2)%10;
+                newStr.append(sum);
+            }
+
+            s = newStr.toString();
+        }
+        return s.charAt(0)== s.charAt(1);
     }
 }
