255646f623e10a73bb474a8f6aacf818db699e97
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-29 10:24:01 +0530

    codE:

---
+++ src/KunalKushwahaAssignment/Others/Problem3370.java
@@ -0,0 +1,33 @@
+package KunalKushwahaAssignment.Others;
+
+public class Problem3370 {
+    public static void main(String[] args) {
+        System.out.println(smallestNumber(10));
+        System.out.println(Integer.bitCount(15));
+    }
+
+    public static int smallestNumber(int n) {
+        String bit = Integer.toBinaryString(n);
+        int bitValue = numberOfOne(bit);
+        while(true){
+            if(numberOfOne(Integer.toBinaryString(n)) == bit.length()){
+                return n;
+            }
+            else{
+                n ++;
+            }
+        }
+
+    }
+
+    public static int numberOfOne(String n){
+        int ans = 0;
+        for (int i = 0; i <n.length() ; i++) {
+            if(n.charAt(i) == '1'){
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+}
