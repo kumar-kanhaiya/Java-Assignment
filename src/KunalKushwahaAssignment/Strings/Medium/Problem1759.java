0a2815c22e07d85e42074d15e672067edc60f88a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-11-14 23:06:03 +0530

    code

--- src/KunalKushwahaAssignment/Strings/Medium/Problem1759.java
+++ src/KunalKushwahaAssignment/Strings/Medium/Problem1759.java
@@ -1,18 +1,29 @@
 package KunalKushwahaAssignment.Strings.Medium;
 
+import java.util.stream.Collectors;
+
 public class Problem1759 {
     public static void main(String[] args) {
         String s = "zzzzz";
-        System.out.println(countHomogenous(s));
+        System.out.println(countHomogenous1(s));
 
     }
 
     public static int countHomogenous(String s) {
         int ans = 0;
+        int mod = 1000000007;
         // if all the element is same then we use n * (n+1)/2 formula
+
+        if(s.chars().mapToObj(c -> (char)c).collect(Collectors.toSet()).size() == 1){
+            int length = s.length();
+
+            ans = (length * (length + 1 ))/2;
+            return ans;
+        }
+
         for (int i = 0; i < s.length() ; i++) {
             String check = s.charAt(i)+"";
-            for(int j = i+1 ; j< s.length() ; j++){
+            for(int j = i ; j< s.length() ; j++){
                 if(s.charAt(i) != s.charAt(j)){
                     break;
                 }
@@ -23,4 +34,23 @@ public class Problem1759 {
 
         return ans;
     }
+
+    //2nd approach
+    public static int countHomogenous1(String s){
+        long mod = 1000000007;
+
+        long ans = 0;
+        for (int i = 0; i <s.length() ;) {
+            long count =0;
+            char ch = s.charAt(i);
+
+            while(i<s.length() && s.charAt(i) == ch){
+                count++;
+                i++;
+            }
+            ans += ((count * (count +1))/2)%mod;
+        }
+        return (int)(ans %mod);
+    }
+
 }
