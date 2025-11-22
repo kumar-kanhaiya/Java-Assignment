e77bd73318b0b4a16abe2f7e1cb6e895f00e261e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-11-22 11:35:35 +0530

    code

--- src/KunalKushwahaAssignment/Others/Problem3190.java
+++ src/KunalKushwahaAssignment/Others/Problem3190.java
@@ -3,7 +3,7 @@ package KunalKushwahaAssignment.Others;
 public class Problem3190 {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-        System.out.println(minimumOperations(arr));
+        System.out.println(minimumOperations2(arr));
 
     }
 
@@ -15,6 +15,20 @@ public class Problem3190 {
             }
             else{
                 continue;
+            }
+        }
+        return ans;
+    }
+
+    // 2nd approach
+    public static int minimumOperations2(int[] arr){
+        int ans = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i] %3 == 0){
+                continue;
+            }
+            else{
+                ans++;
             }
         }
         return ans;
