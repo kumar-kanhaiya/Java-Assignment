64e53b4c46f86193173985568bffe25b551fb4ea
kumar-kanhaiya <kanhaiyakumarmailme@gmail.com>
2026-02-12 19:22:26 +0530

    reverse String leetcode problem

---
+++ src/KunalKushwahaAssignment/Recursion/ReverseString.java
@@ -0,0 +1,28 @@
+package KunalKushwahaAssignment.Recursion;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    static void main() {
+        char[] ch = {'p','o','q'};
+        reverseString(ch);
+        System.out.println(Arrays.toString(ch));
+
+    }
+
+    public static void reverseString(char[] s) {
+        helper(s,0,s.length-1);
+    }
+
+    public static void helper(char[] s , int start , int end){
+        // base condition
+        if(start == end || start> end ){
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end]= temp;
+
+        helper(s,start+1 , end- 1);
+    }
+}
