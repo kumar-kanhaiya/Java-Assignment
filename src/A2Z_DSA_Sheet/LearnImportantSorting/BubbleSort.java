014255603e4e4151c901c7be627cafd4cccd5f79
kumar-kanhaiya <kanhaiyakumarmailme@gmail.com>
2026-03-03 12:28:22 +0530

    sorting algo

---
+++ src/A2Z_DSA_Sheet/LearnImportantSorting/BubbleSort.java
@@ -0,0 +1,42 @@
+package A2Z_DSA_Sheet.LearnImportantSorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    static void main() {
+        int[] arr = {13,24,20,9,46,52};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = i+1; j <= arr.length - i -1; j++) {
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i]= arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    // optimise approch
+    public static void optimiseBubbleSort(int[] arr){
+        for (int i = arr.length -1; i >=0 ; i--) {
+            int didSwap = 0;
+            for (int j = 0; j <= i-1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1]= arr[j];
+                    arr[j] = temp;
+                    didSwap = 1;
+                }
+            }
+            if(didSwap ==0){
+                break;
+            }
+        }
+    }
+}
---
+++ src/A2Z_DSA_Sheet/LearnImportantSorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package A2Z_DSA_Sheet.LearnImportantSorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    static void main() {
+        int[] arr = {13,46,24,52,20,9};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    // normal approach
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length-1 ; i++) {
+            int min = minimumNumberIndex(arr,i,arr.length);
+            int temp = arr[i];
+            arr[i]= arr[min];
+            arr[min] = temp;
+        }
+
+    }
+    public static int minimumNumberIndex(int[] arr , int start , int end){
+        int min = start;
+        for (int i = start+1; i < end ; i++) {
+            if(arr[i] < arr[min]){
+                min = i;
+            }
+        }
+        return min;
+    }
+}
