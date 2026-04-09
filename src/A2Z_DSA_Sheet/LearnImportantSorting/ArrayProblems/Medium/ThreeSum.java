141d488482811756d09d9c5eb338b4fde3461096
kumar-kanhaiya <kanhaiyakumarmailme@gmail.com>
2026-04-09 15:50:00 +0530

    three sum'

--- src/A2Z_DSA_Sheet/LearnImportantSorting/ArrayProblems/Medium/ThreeSum.java
+++ src/A2Z_DSA_Sheet/LearnImportantSorting/ArrayProblems/Medium/ThreeSum.java
@@ -1,37 +1,34 @@
 package A2Z_DSA_Sheet.LearnImportantSorting.ArrayProblems.Medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ThreeSum {
     static void main(String[] args) {
         int[] arr = {-1,0,1,2,-1,-4};
-        System.out.println(threeSum(arr));
+        System.out.println(threeSum1(arr));
 
     }
     // first normal and brute force approach
     public static List<List<Integer>> threeSum1(int[] arr) {
-        List<List<Integer>> ans = new ArrayList<>();
+//        List<List<Integer>> ans = new ArrayList<>();
         // we have to solve this problem by using three loops
+        // store in the set
+        Set<List<Integer>> st = new HashSet<>();
         // first loop
         for (int i = 0; i < arr.length ; i++) {
             for (int j = i+1; j < arr.length ; j++) {
                 for (int k = j+1; k < arr.length ; k++) {
-                    List<Integer> check = new ArrayList<>();
-                    if(arr[i] + arr[j] + arr[k] == 0){
-                        check.add(arr[i]);
-                        check.add(arr[j]);
-                        check.add(arr[k]);
-                    }
-                    if(check.size() > 1){
-                        ans.add(check);
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        // Store sorted triplet to avoid duplicates
+                        List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                        Collections.sort(temp);
+                        st.add(temp);
                     }
                 }
             }
 
         }
-        return ans;
+        return new ArrayList<>(st);
     }
 
     // second approach
@@ -65,4 +62,60 @@ public class ThreeSum {
         }
         return ans;
     }
+
+    public static List<List<Integer>> threeSum2(int[] arr){
+        Set<List<Integer>> ans = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            // new hashset
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i+1; j < arr.length ; j++) {
+                int third = -(arr[i] + arr[j]);
+                if(hashset.contains(third)){
+                    List<Integer> temp = Arrays.asList(arr[i] , arr[j] , third);
+                    Collections.sort(temp);
+                    ans.add(temp);
+                }
+                hashset.add(arr[j]);
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+
+    // approach with using binary search
+    public static List<List<Integer>> threeSum3(int[] arr){
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        // sort the array
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = i+1; j < arr.length ; j++) {
+                int target = -(arr[i] + arr[j]);
+                int k = binarySearch(arr , target , j+1 , arr.length -1);
+                if(k!= -1){
+                    List<Integer> triplet = Arrays.asList(arr[i], arr[j], arr[k]);
+                    set.add(triplet);
+                }
+            }
+
+        }
+        ans.addAll(set);
+        return ans;
+    }
+
+    public static int binarySearch(int[] arr , int target , int start , int end){
+        while(start<= end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(arr[mid] > target){
+                end = mid -1;
+            }
+            else{
+                start = mid +1;
+            }
+        }
+        return -1;
+    }
+
 }
